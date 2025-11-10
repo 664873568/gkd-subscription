693414e49d542283d43059b51a421a899187d8fa
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2025-11-10 17:43:23 +0800

    Update subscription ID and author name

--- src/subscription.ts
+++ src/subscription.ts
@@ -4,10 +4,10 @@ import categories from './categories';
 import globalGroups from './globalGroups';
 
 export default defineGkdSubscription({
-  id: 233,
+  id: 664873568,
   name: 'Subscription',
   version: 0,
-  author: 'author',
+  author: '蓝蝶绯翼',
   checkUpdateUrl: './gkd.version.json5',
   supportUri: 'https://github.com/gkd-kit/subscription-template',
   categories,
