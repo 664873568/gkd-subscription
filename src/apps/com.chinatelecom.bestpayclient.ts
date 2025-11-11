df5ec79e612ed2cc3a2ff365aa5f936b38ada8fb
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2025-11-11 19:41:15 +0800

    Update com.chinatelecom.bestpayclient.ts

--- src/apps/com.chinatelecom.bestpayclient.ts
+++ src/apps/com.chinatelecom.bestpayclient.ts
@@ -6,13 +6,14 @@ export default defineGkdApp({
   groups: [
     {
       key: 1,
-      name: '看视频-跳过-西瓜视频',
+      name: '看视频-跳过',
       matchRoot: true,
       actionMaximum: 1,
       matchTime: 10000,
       resetMatch: 'activity',
       rules: [
         {
+          action: 'clickCenter',
           matches: [
             '@[text*="我要" || text*="体验"] < * -n [text*="广告" || text*="可"] - [text*="秒"] - [text*="看" || text="去浏览"]',
           ],
@@ -33,7 +34,7 @@ export default defineGkdApp({
     },
     {
       key: 2,
-      name: '看视频-跳过-京东',
+      name: '看视频-跳过-获得加速机会',
       matchRoot: true,
       actionMaximum: 1,
       matchTime: 10000,
@@ -41,6 +42,7 @@ export default defineGkdApp({
       rules: [
         {
           key: 0,
+          action: 'clickCenter',
           matches: [
             '@[text="我要加速领奖" || text="返回继续体验"] -2 [text="get_reward_btn_bg"] <<n * -n * >n [text="get_accelerate_chance"]',
           ],
@@ -54,6 +56,7 @@ export default defineGkdApp({
         },
         {
           key: 1,
+          action: 'clickCenter',
           matches: [
             '@[text="我要免看本次广告"] < * -n * > [text="恭喜获得特权"]',
           ],
@@ -64,6 +67,73 @@ export default defineGkdApp({
     },
     {
       key: 3,
+      name: '看视频-跳过-拿奖励',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'activity',
+      rules: [
+        {
+          action: 'clickCenter',
+          matches: [
+            '@[text="拿奖励"] < * -n * > [text*="可"] -n [text*="秒"] - [text*="看"]',
+          ],
+          snapshotUrls: [
+            'https://i.gkd.li/i/23475883',
+          ],
+          activityIds: [
+            'com.kwad.sdk.api.proxy.app.KsRewardVideoActivity',
+          ],
+        },
+      ],
+    },
+    {
+      key: 4,
+      name: '看视频-跳过-礼盒',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'activity',
+      rules: [
+        {
+          action: 'clickCenter',
+          matches: [
+            '@[text="跳过"] <<n * - * > [desc="gift_box"] > ImageView',
+          ],
+          snapshotUrls: [
+            'https://i.gkd.li/i/23476056',
+            'https://i.gkd.li/i/23476161',
+          ],
+          activityIds: [
+            'com.kwad.sdk.api.proxy.app.KsRewardVideoActivity',
+          ],
+        },
+      ],
+    },
+    {
+      key: 5,
+      name: '看视频-跳过',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'activity',
+      rules: [
+        {
+          action: 'clickCenter',
+          matches: [
+            '@ImageView <<n [desc="close_button"] - * < * - * > [desc="gift_box"] > ImageView',
+          ],
+          snapshotUrls: [
+            'https://i.gkd.li/i/23476430',
+          ],
+          activityIds: [
+            'com.kwad.sdk.api.proxy.app.KsRewardVideoActivity',
+          ],
+        },
+      ],
+    },
+    {
+      key: 7,
       name: '看视频-跳过-立即下载',
       matchRoot: true,
       actionMaximum: 1,
@@ -71,6 +141,7 @@ export default defineGkdApp({
       resetMatch: 'activity',
       rules: [
         {
+          action: 'clickCenter',
           matches: [
             '@ImageView + [text="应用详情"] <<n * +n *[text^="立即下载"]',
           ],
@@ -82,7 +153,7 @@ export default defineGkdApp({
       ],
     },
     {
-      key: 4,
+      key: 8,
       name: '看视频-跳过-奖励已领取',
       matchRoot: true,
       actionMaximum: 1,
@@ -90,6 +161,7 @@ export default defineGkdApp({
       resetMatch: 'activity',
       rules: [
         {
+          action: 'clickCenter',
           matches: ['@[text="｜跳过"] - [text="奖励已领取"]'],
           snapshotUrls: [
             'https://i.gkd.li/i/23451963',
@@ -102,7 +174,7 @@ export default defineGkdApp({
       ],
     },
     {
-      key: 5,
+      key: 9,
       name: '看视频-再得*金币',
       matchRoot: true,
       actionMaximum: 1,
@@ -110,6 +182,7 @@ export default defineGkdApp({
       resetMatch: 'activity',
       rules: [
         {
+          action: 'clickCenter',
           matches: ['@[text^="看视频再得"] -2 [text^="+"]'],
           snapshotUrls: [
             'https://i.gkd.li/i/23453300',
