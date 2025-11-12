57abdd446893c54f3858ce280595b789f56a12d1
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2025-11-12 15:42:44 +0800

    Create com.miui.securitycenter.ts

---
+++ src/apps/com.miui.securitycenter.ts
@@ -0,0 +1,29 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'com.miui.securitycenter',
+  name: '安全服务',
+  groups: [
+    {
+      key: 0,
+      name: '看视频-去领奖-启动应用',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'activity',
+      rules: [
+        {
+          action: 'back',
+          actionDelay: 15000,
+          matches: ['[text="启动应用"] + [text^="翼支付"]'],
+          snapshotUrls: [
+            'https://i.gkd.li/i/23494155',
+          ],
+          activityIds: [
+            'com.miui.wakepath.ui.ConfirmStartActivity',
+          ],
+        },
+      ],
+    },
+  ],
+});
