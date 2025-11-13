8a347838b1ca368da65023857e3845706cc5dee9
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2025-11-13 15:22:34 +0800

    Update com.jd.jrapp.ts

--- src/apps/com.jd.jrapp.ts
+++ src/apps/com.jd.jrapp.ts
@@ -20,7 +20,8 @@ export default defineGkdApp({
           snapshotUrls: [
             'https://i.gkd.li/i/23397450',
             'https://i.gkd.li/i/23424253',
-          ],
+            ],
+          activityIds: ['.bm.common.web.ui.WebActivity'],
         },
       ],
     },
