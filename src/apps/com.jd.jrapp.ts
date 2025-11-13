cb9f8cb8bee4cbd0d6a2428c09ddfe19df7df7b4
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2025-11-13 15:18:17 +0800

    Update com.jd.jrapp.ts

--- src/apps/com.jd.jrapp.ts
+++ src/apps/com.jd.jrapp.ts
@@ -5,8 +5,8 @@ export default defineGkdApp({
   name: '京东金融',
   groups: [
     {
-      key: 1,
-      name: '跳转中-跳转中-<',
+      key: 0,
+      name: '跳转中-跳转中...',
       matchRoot: true,
       actionMaximum: 1,
       matchTime: 10000,
@@ -14,21 +14,13 @@ export default defineGkdApp({
       rules: [
         {
           key: 0,
-          action: 'click',
+          action: 'back',
           actionDelay: 1000,
-          name: '跳转中-<',
-          matches: ['@Button < * + [text="跳转中"]'],
-          snapshotUrls: ['https://i.gkd.li/i/23397450'],
-          activityIds: ['.bm.common.web.ui.WebActivity'],
-        },
-        {
-          key: 1,
-          action: 'click',
-          actionDelay: 1000,
-          name: '跳转中-×',
-          matches: ['@Button <2 * + [text="跳转中"]'],
-          snapshotUrls: ['https://i.gkd.li/i/23424253'],
-          activityIds: ['.bm.common.web.ui.WebActivity'],
+          matches: ['@[text="跳转中"] <<n * +n * >n [text="跳转中..."]'],
+          snapshotUrls: [
+            'https://i.gkd.li/i/23397450',
+            'https://i.gkd.li/i/23424253',
+          ],
         },
       ],
     },
