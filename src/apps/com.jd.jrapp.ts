ddc6963fa9d3f2bf7b11aa4140c583e4be983f2c
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2025-11-15 12:04:22 +0800

    Update com.jd.jrapp.ts

--- src/apps/com.jd.jrapp.ts
+++ src/apps/com.jd.jrapp.ts
@@ -25,25 +25,5 @@ export default defineGkdApp({
         },
       ],
     },
-    {
-      key: 1,
-      name: '黑色星期五-领券抢',
-      matchRoot: true,
-      forcedTime: 1000,
-      actionMaximum: 100,
-      actionCd: 100,
-      resetMatch: 'activity',
-      rules: [
-        {
-          key: 0,
-          action: 'clickCenter',
-          matches: [
-            '@[text="领券抢"] <<n [text="黑色星期五"] >n [text="15枚无抗鲜蛋"]',
-          ],
-          snapshotUrls: ['https://i.gkd.li/i/23548893'],
-          activityIds: ['com.jd.lib.babel.view.activity.BabelActivity'],
-        },
-      ],
-    },
   ],
 });
