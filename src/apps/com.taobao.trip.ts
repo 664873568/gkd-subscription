63b979d47aeae799baca83bda61fc72659a0b787
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2025-11-15 21:33:49 +0800

    Create com.taobao.trip.ts

---
+++ src/apps/com.taobao.trip.ts
@@ -0,0 +1,471 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'com.taobao.trip',
+  name: '飞猪旅行',
+  groups: [
+    //天天集能量
+    {
+      key: 0,
+      name: '任务中心-点击赚积分',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'activity',
+      rules: [
+        {
+          matches: ['@[text="看视频赚钱"] <<n [text="天天集能量"]'],
+          snapshotUrls: [
+            'https://i.gkd.li/i/23561327',
+            'https://i.gkd.li/i/23561328',
+            ],
+          activityIds: ['fliggyx.android.unicorn.ActWebviewActivity'],
+        },
+      ],
+    },
+    //看视频
+    {//*暂未适配*//
+      key: 10,
+      name: '看视频-跳过↑',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'activity',
+      rules: [
+        {
+          anyMatches: [
+            '@[text^="我要"] <<n * -n * >n [text$="跳过"]',
+            '@[text$="体验"] <<n * -n * >n [text$="跳过"]',
+            '@[text="立即前往"] <<n * -n * >n [text$="跳过"]',
+            '@[text="再试一次"] <<n * -n * >n [text$="跳过"]',
+          ],
+          snapshotUrls: [
+            'https://i.gkd.li/i/23545855', //去体验-左下[width=711 && height=183]
+          ],
+          activityIds: [
+            'com.bytedance.sdk.openadsdk.core.component.reward.activity.TTRewardVideoActivity',
+          ],
+        },
+      ],
+    },
+    {
+      key: 11,
+      name: '看视频-跳过↓',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'activity',
+      rules: [
+        {
+          anyMatches: [
+            '@[text^="我要"] <<n * +n * >n [text$="跳过"]',
+            '@[text$="体验"] <<n * +n * >n [text$="跳过"]',
+            '@[text^="立即"] <<n * +n * >n [text$="跳过"]',
+            '@[text="再试一次"] <<n * +n * >n [text$="跳过"]',
+          ],
+          snapshotUrls: [
+            'https://i.gkd.li/i/23561339', //我要加速
+            'https://i.gkd.li/i/23561353', //再试一次
+          ],
+          activityIds: [
+            'com.bytedance.sdk.openadsdk.stub.activity.Stub_Standard_Portrait_Activity',
+          ],
+        },
+      ],
+    },
+    {
+      //*暂未适配*//
+      key: 111,
+      name: '看视频-跳过↓-体验',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'activity',
+      rules: [
+        {
+          matches: [
+            '[text="可提前20秒领奖"] -n @[text$="体验"] <<n * +n * >n [text$="跳过"]',
+          ],
+          snapshotUrls: [
+            'https://i.gkd.li/i/23518146', //去体验
+            'https://i.gkd.li/i/23518156', //返回继续体验
+          ],
+          activityIds: [
+            'com.kwad.sdk.api.proxy.app.KsRewardVideoActivity',
+            'com.bytedance.sdk.openadsdk.stub.activity.Stub_Standard_Portrait_Activity',
+          ],
+        },
+      ],
+    },
+    {//*暂未适配*//
+      key: 12,
+      name: '看视频-跳过-×-应用详情+立即下载',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 20000,
+      resetMatch: 'activity',
+      rules: [
+        {
+          actionDelay: 15000,
+          matches: [
+            '@ImageView + [text="应用详情"] <<n * +n *[text="立即下载"]',
+          ],
+          snapshotUrls: ['https://i.gkd.li/i/23559186'],
+          activityIds: [
+            'com.bytedance.sdk.openadsdk.core.component.reward.activity.TTRewardVideoActivity',
+          ],
+        },
+      ],
+    },
+    {
+      //*暂未适配*//
+      key: 13,
+      name: '看视频-跳过-继续观看',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'activity',
+      rules: [
+        {
+          matches: ['@[text="继续观看"] <<n * +n * >n [text="跳过"]'],
+          snapshotUrls: ['https://i.gkd.li/i/23497387'],
+          activityIds: ['com.kwad.sdk.api.proxy.app.KsRewardVideoActivity'],
+        },
+      ],
+    },
+    {
+      //*暂未适配*//
+      key: 14,
+      name: '看视频-长按-×', //长按无法实现
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'activity',
+      rules: [
+        {
+          matches: [
+            '@ImageView <<n * -n * >n [text="长按加速视频 获取奖励"]', //@[desc^="reward-playback-speed_playSpeed"] >n [text="长按加速视频 获取奖励"]
+          ],
+          snapshotUrls: [
+            'https://i.gkd.li/i/23497570',
+            'https://i.gkd.li/i/23498065',
+          ],
+          activityIds: ['com.kwad.sdk.api.proxy.app.KsRewardVideoActivity'],
+        },
+      ],
+    },
+    {//*暂未适配*//
+      key: 15,
+      name: '看视频-去领奖',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'activity',
+      rules: [
+        {
+          matches: ['@[text="去领奖"] <<n * -n * >n [text="svg%3e"]'],
+          snapshotUrls: [
+            'https://i.gkd.li/i/23547463',
+            'https://i.gkd.li/i/23547568',
+            'https://i.gkd.li/i/23547641',
+            'https://i.gkd.li/i/23559530',
+          ],
+          activityIds: [
+            'com.bytedance.sdk.openadsdk.core.component.reward.activity.TTRewardVideoActivity',
+          ],
+        },
+      ],
+    },
+    //看视频-返回|跳过|关闭
+    {//*暂未适配*//
+      key: 16,
+      name: '看视频-去领奖-奖励已领取',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'activity',
+      rules: [
+        {
+          action: 'back',
+          matches: [
+            '@RelativeLayout <<n * + * >n [text="奖励已领取"] - [text="svg%3e"]',
+          ],
+          snapshotUrls: [
+            'https://i.gkd.li/i/23547705',
+            'https://i.gkd.li/i/23547759',
+          ],
+          activityIds: [
+            'com.bytedance.sdk.openadsdk.core.component.reward.activity.TTRewardVideoActivity',
+          ],
+        },
+      ],
+    },
+    {
+      key: 17,
+      name: '看视频-跳过-奖励已领取',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'activity',
+      rules: [
+        {
+          actionDelay: 100,
+          matches: ['@[text$="跳过"] -n [text="奖励已领取"]'],
+          snapshotUrls: ['https://i.gkd.li/i/23561361'],
+          activityIds: [
+            'com.bytedance.sdk.openadsdk.stub.activity.Stub_Standard_Portrait_Activity',
+          ],
+        },
+      ],
+    },
+    {
+      //*暂未适配*//
+      key: 18,
+      name: '看视频-礼包-领取奖励',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 30000,
+      resetMatch: 'activity',
+      rules: [
+        {
+          actionDelay: 100,
+          matches: [
+            '@[text="跳过" || desc="close_button"] <<n * -n * >n [desc="gift_box"]',
+          ],
+          snapshotUrls: [
+            'https://i.gkd.li/i/23476056', //跳过-继续体验
+            'https://i.gkd.li/i/23476161', //跳过
+            'https://i.gkd.li/i/23476430', //免费获取>×
+          ],
+          activityIds: ['com.kwad.sdk.api.proxy.app.KsRewardVideoActivity'],
+        },
+      ],
+    },
+    {//*暂未适配*//
+      key: 19,
+      name: '看视频-跳过-×',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'activity',
+      rules: [
+        {
+          matches: ['@ImageView <<n * -n * >n [text="反馈"]'],
+          snapshotUrls: ['https://i.gkd.li/i/23545935'],
+          activityIds: [
+            'com.bytedance.sdk.openadsdk.core.component.reward.activity.TTRewardVideoActivity',
+          ],
+        },
+      ],
+    },
+    {//*暂未适配*//
+      key: 20,
+      name: '看视频-温馨提示-去领取奖励',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'activity',
+      rules: [
+        {
+          matches: ['@[text="去领取奖励"] -n * >n [text="温馨提示"]'],
+          snapshotUrls: ['https://i.gkd.li/i/23547170'],
+          activityIds: [
+            'com.bytedance.sdk.openadsdk.core.component.reward.activity.TTRewardVideoActivity',
+          ],
+        },
+      ],
+    },
+    {
+      key: 21,
+      name: '看视频-限时特权 免看广告',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'activity',
+      rules: [
+        {
+          matches: ['@[vid="anythink_myoffer_inctask_cta"] <<n * +n * > [vid="anythink_myoffer_btn_close_id"]'],
+          snapshotUrls: [
+            'https://i.gkd.li/i/23561418',
+            'https://i.gkd.li/i/23561474',
+            ],
+          activityIds: [
+            'com.anythink.basead.ui.ATPortraitActivity',
+            'null',
+          ],
+        },
+      ],
+    },
+    {
+      key: 22,
+      name: '看视频-限时特权 免看广告-×',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'activity',
+      rules: [
+        {
+          matches: ['ImageView +2 * > [vid="anythink_myoffer_btn_close_id"]'],
+          snapshotUrls: ['https://i.gkd.li/i/23561459'],
+          activityIds: [
+            'com.anythink.basead.ui.ATPortraitActivity',
+          ],
+        },
+      ],
+    },
+    {
+      key: 23
+      name: '看视频-完成APP下载-即可领取奖励',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'activity',
+      activityIds: ['com.qumeng.advlib.ui.front.InciteADActivity'],
+      rules: [
+        {
+          key: 0,
+          actionDelay: 1000,
+          matches: ['@ImageView - RelativeLayout > View <<n [vid="action_bar_root"] >n [text="  广告"] + [text*="点众快看"]'],
+          snapshotUrls: ['https://i.gkd.li/i/23561396'],
+        },
+        {
+          preKeys: 0,
+          key: 1,
+          matches: ['@[text="关闭广告"] - [text="去下载拿奖励"] - [text="只要完成APP下载，即可领取奖励"]'],
+          snapshotUrls: ['https://i.gkd.li/i/23561405'],
+        },
+      ],
+    },
+    {
+      key: 24,
+      name: '看视频-精选推荐',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'activity',
+      rules: [
+        {
+          matches: ['@ImageView - [text="反馈"] +n * > [text="精选推荐"]'],
+          snapshotUrls: [
+            'https://i.gkd.li/i/23561480',
+            ],
+          activityIds: [
+            'com.baidu.mobads.sdk.api.MobRewardVideoActivity',
+          ],
+        },
+      ],
+    },
+    //看视频-二级
+    {
+      key: 31,
+      name: '看视频-跳转快应用-×',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'activity',
+      rules: [
+        {
+          action: 'back',
+          matches: ['@ImageView + ImageView +n [text="反馈"]'],
+          snapshotUrls: ['https://i.gkd.li/i/23547990'],
+          activityIds: [
+            'com.bytedance.sdk.openadsdk.core.activity.base.TTWebPageActivity',
+            'null',
+          ],
+        },
+      ],
+    },
+    //首页功能类
+    {
+      key: 40,
+      name: '开始安全备份',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'app',
+      rules: [
+        {
+          action: 'back',
+          anyMatches: [
+            '@[text="开始安全备份"] <<n [vid="cl_open_auto_backup"]',
+          ],
+          snapshotUrls: ['https://i.gkd.li/i/23558701'],
+          activityIds: ['.home.view.HomeActivity'],
+        },
+      ],
+    },
+    {
+      key: 41,
+      name: '开启自定义备份',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'app',
+      rules: [
+        {
+          anyMatches: ['@[vid="iv_close"] +n [text="开启自定义备份"]'],
+          snapshotUrls: ['https://i.gkd.li/i/23558728'],
+          activityIds: ['.home.view.HomeActivity'],
+        },
+      ],
+    },
+    {
+      key: 42,
+      name: '立即升级',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'app',
+      rules: [
+        {
+          anyMatches: ['@[vid="img_close"] -n * >n [text="立即升级"]'],
+          snapshotUrls: ['https://i.gkd.li/i/23558731'],
+          activityIds: ['.home.view.HomeActivity'],
+        },
+      ],
+    },
+    //首页广告类
+    {
+      key: 50,
+      name: '首页广告-×',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'app',
+      rules: [
+        {
+          matches: [
+            '@[vid="dialog_home_pop_close_button"] + [vid="img_thumbnail"]',
+          ],
+          snapshotUrls: [
+            'https://i.gkd.li/i/23558690',
+            'https://i.gkd.li/i/23558692',
+          ],
+          activityIds: ['.home.view.HomeActivity'],
+        },
+      ],
+    },
+    //会员支付类
+    {
+      key: 60,
+      name: '随心卡会员,',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'app',
+      rules: [
+        {
+          matches: [
+            '@TextView -n * >n [text="立即使用"] <<n [text="随心卡会员"]',
+            '@Image -n * >n [text="立即开通"] <<n [text="随心卡会员"]',
+          ],
+          snapshotUrls: [
+            'https://i.gkd.li/i/23558756', //立即使用
+            'https://i.gkd.li/i/23558692', //立即开通
+          ],
+          activityIds: ['.home.view.HomeActivity'],
+        },
+      ],
+    },
+  ],
+});
