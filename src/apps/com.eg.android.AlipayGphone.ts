33da55a78749895f1a24009a7e63be83735b2ac2
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2025-11-16 16:06:37 +0800

    Update com.eg.android.AlipayGphone.ts

--- src/apps/com.eg.android.AlipayGphone.ts
+++ src/apps/com.eg.android.AlipayGphone.ts
@@ -9,7 +9,7 @@ export default defineGkdApp({
       name: '网商财神-开红包',
       matchRoot: true,
       actionMaximum: 1,
-      matchTime: 3000,
+      matchTime: 5000,
       resetMatch: 'activity',
       activityIds: ['com.alipay.mobile.nebulax.xriver.activity.XRiverActivity'],
       rules: [
@@ -29,7 +29,7 @@ export default defineGkdApp({
       name: '网商财神-开红包-×',
       matchRoot: true,
       actionMaximum: 1,
-      matchTime: 3000,
+      matchTime: 5000,
       resetMatch: 'activity',
       activityIds: ['com.alipay.mobile.nebulax.xriver.activity.XRiverActivity'],
       rules: [
@@ -42,6 +42,28 @@ export default defineGkdApp({
             'https://i.gkd.li/i/23561801',
             'https://i.gkd.li/i/23561802',
             'https://i.gkd.li/i/23561803',
+          ],
+          activityIds: [
+            'com.alipay.mobile.nebulax.xriver.activity.XRiverActivity',
+          ],
+        },
+      ],
+    },
+    {
+      key: 2,
+      name: '网商财神-开红包-完成任务',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'activity',
+      activityIds: ['com.alipay.mobile.nebulax.xriver.activity.XRiverActivity'],
+      rules: [
+        {
+          matches: [
+            '@[text="Smallfish App"] >n [text="任务完成"]',
+          ],
+          snapshotUrls: [
+            'https://i.gkd.li/i/23566578',
           ],
           activityIds: [
             'com.alipay.mobile.nebulax.xriver.activity.XRiverActivity',
