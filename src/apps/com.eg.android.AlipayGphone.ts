bc17b1908ca3ab25101e7f59f2ef69942bc74a13
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2025-11-16 15:58:29 +0800

    Update com.eg.android.AlipayGphone.ts

--- src/apps/com.eg.android.AlipayGphone.ts
+++ src/apps/com.eg.android.AlipayGphone.ts
@@ -14,15 +14,26 @@ export default defineGkdApp({
       activityIds: ['com.alipay.mobile.nebulax.xriver.activity.XRiverActivity'],
       rules: [
         {
-          key: 0,
           matches: [
             '@View -n * <<3 [id="ant-render-id-CPT_67f7a08a088a0168e4b61c05"]',
           ],
           snapshotUrls: ['https://i.gkd.li/i/23561800'],
+          activityIds: [
+            'com.alipay.mobile.nebulax.xriver.activity.XRiverActivity',
+          ],
         },
+      ],
+    },
+    {
+      key: 1,
+      name: '网商财神-开红包',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 3000,
+      resetMatch: 'activity',
+      activityIds: ['com.alipay.mobile.nebulax.xriver.activity.XRiverActivity'],
+      rules: [
         {
-          preKeys: 0,
-          key: 1,
           anyMatches: [
             '@TextView -n * < * -n * <<n [id="ant-render-id-CPT_67f7a08a088a0168e4b61c05"]',
             '@TextView -n * < * -n * < * < * -n * <<n [id="ant-render-id-CPT_67f7a204088a0168e4b61c07"]',
@@ -31,6 +42,9 @@ export default defineGkdApp({
             'https://i.gkd.li/i/23561801',
             'https://i.gkd.li/i/23561802',
             'https://i.gkd.li/i/23561803',
+          ],
+          activityIds: [
+            'com.alipay.mobile.nebulax.xriver.activity.XRiverActivity',
           ],
         },
       ],
