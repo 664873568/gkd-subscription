eeb6861eed020a1891db06fc80896f53035647b4
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2025-12-23 08:56:40 +0800

    Create com.suning.mobile.epa.ts

---
+++ src/apps/com.suning.mobile.epa.ts
@@ -0,0 +1,24 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'com.suning.mobile.epa',
+  name: '星图金融',
+  groups: [
+    {
+      key: 0,
+      name: '首页-领现金',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'activity',
+      rules: [
+        {
+          matches: [
+            '@[text="领现金"] - * > [vid="item_img"]',
+          ],
+          activityIds: ['.launcher.LauncherActivity'],
+        },
+      ],
+    },
+  ],
+});
