e13eb951d813ca6a756beee2e1c031312ea1c634
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2025-12-23 09:01:54 +0800

    Create com.xiaomi.market.ts

---
+++ src/apps/com.xiaomi.market.ts
@@ -0,0 +1,25 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'com.xiaomi.market',
+  name: '应用商店',
+  groups: [
+    {
+      key: 0,
+      name: '应用商店-小米应用商店-关闭',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 20000,
+      resetMatch: 'activity',
+      rules: [
+        {
+          actionDelay: 16000,
+          anyMatches: [
+            '@[desc="关闭"] -n * >n [text="小米应用商店"]',
+          ],
+          activityIds: ['null'],
+        },
+      ],
+    },
+  ],
+});
