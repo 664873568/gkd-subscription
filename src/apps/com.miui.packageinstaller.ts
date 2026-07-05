3e0277f4927b25498be6c677b099ae9dd4ebc164
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2026-07-05 20:47:47 +0800

    Update com.miui.packageinstaller.ts

--- src/apps/com.miui.packageinstaller.ts
+++ src/apps/com.miui.packageinstaller.ts
@@ -33,9 +33,22 @@ export default defineGkdApp({
             '@[text="禁止安装"] <<n * [text^="是否允许"]',
             '@[text="安装包扫描中，请稍候"] <<n * [vid="main_content"]',
           ],
-          activityIds: [
-            'com.miui.packageInstaller.NewInstallerPrepareActivity',
-          ],
+          activityIds: ['com.miui.packageInstaller.NewInstallerPrepareActivity',],
+        },
+      ],
+    },
+    {
+      key: 2,
+      name: '安装应用',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 20000,
+      resetMatch: 'activity',
+      rules: [
+        {
+          actionDelay: 15000,
+          matches: ['@[text="取消"] <<n * [text$="频繁安装应用"]'],
+          activityIds: ['null'],
         },
       ],
     },
