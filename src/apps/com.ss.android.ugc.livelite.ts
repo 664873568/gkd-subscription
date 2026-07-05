538f81797c78ae0b93bf8436f98bf66c4d379ebd
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2026-07-05 22:29:04 +0800

    Create com.ss.android.ugc.livelite.ts

---
+++ src/apps/com.ss.android.ugc.livelite.ts
@@ -0,0 +1,54 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'com.ss.android.ugc.livelite',
+  name: '抖音商城',
+  groups: [
+    {
+      key: 0,
+      name: '浏览好物-返回领取',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 40000,
+      resetMatch: 'activity',
+      rules: [
+        {
+          action: 'back',
+          matches: ['@[text="返回领取"] <<n * [id="recharge-pendant"]'],
+          activityIds: ['com.ss.android.ugc.aweme.live.LiveDummyActivity'],
+        },
+      ],
+    },
+    {
+      key: 1,
+      name: '看视频-返回领取',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 40000,
+      resetMatch: 'activity',
+      rules: [
+        {
+          excludeMatches: ['ImageView - @ImageView < ViewGroup -3 ScrollView'],
+          matches: ['@ImageView < ViewGroup -3 ScrollView'],
+          activityIds: ['com.ss.android.excitingvideo.ExcitingVideoActivity'],
+        },
+      ],
+    },
+    {
+      key: 2,
+      name: '看视频-直播-返回',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 40000,
+      resetMatch: 'activity',
+      rules: [
+        {
+          action: 'back',
+          actionDelay: 30000,
+          matches: ['@[vid="cso"] <<n * LiveMeasureOnceRelativeLayout'],
+          activityIds: ['com.ss.android.ugc.aweme.live.LivePlayActivity'],
+        },
+      ],
+    },
+  ],
+});
