8a8d5091316398c7e6ce129dc2bd1e1a5c0b94be
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2026-07-07 07:05:33 +0800

    Update com.duowan.mobile.ts

--- src/apps/com.duowan.mobile.ts
+++ src/apps/com.duowan.mobile.ts
@@ -9,12 +9,26 @@ export default defineGkdApp({
       name: '打开通知权限-×',
       matchRoot: true,
       actionMaximum: 1,
-      matchTime: 20000,
+      matchTime: 10000,
       resetMatch: 'activity',
       rules: [
         {
           matches: ['@[vid="iv_close"] <<n * [text="打开通知权限"]'],
           activityIds: ['.basemedia.watchlive.activity.LiveTemplateActivity'],
+        },
+      ],
+    },
+    {
+      key: 1,
+      name: '发现新版本-立即下载-×',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'activity',
+      rules: [
+        {
+          matches: ['@[vid="btn_cancel"] <<n * [vid="btn_ok"]'],
+          activityIds: ['com.yy.mobile.ui.common.JsSupportWebAcitivity'],
         },
       ],
     },
