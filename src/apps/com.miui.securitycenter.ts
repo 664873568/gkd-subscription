e94fbc9bd3867fc4cf3db78c63e5f0ddc3b871c5
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2026-07-07 05:30:24 +0800

    Update com.miui.securitycenter.ts

--- src/apps/com.miui.securitycenter.ts
+++ src/apps/com.miui.securitycenter.ts
@@ -2,7 +2,7 @@ import { defineGkdApp } from '@gkd-kit/define';
 
 export default defineGkdApp({
   id: 'com.miui.securitycenter',
-  name: '安全服务', //手机管家
+  name: '安全服务',
   groups: [
     {
       key: 0,
@@ -69,4 +69,75 @@ export default defineGkdApp({
       ],
     },
   ],
-});
+},
+{
+  id: 'com.miui.securitycenter',
+  name: '手机管家',
+  groups: [
+    {
+      key: 0,
+      name: '启动应用',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 20000,
+      resetMatch: 'activity',
+      rules: [
+        {
+          action: 'back',
+          actionDelay: 16000,
+          matches: ['@[text="启动应用"] + [text$="是否允许？"]'],
+          activityIds: ['com.miui.wakepath.ui.ConfirmStartActivity', 'null'],
+        },
+      ],
+    },
+    {
+      key: 1,
+      name: '请用指纹解锁',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 20000,
+      resetMatch: 'activity',
+      rules: [
+        {
+          action: 'back',
+          actionDelay: 16000,
+          matches: ['@[text="请用指纹解锁"] - [vid="app_icon"]'],
+          activityIds: ['com.miui.applicationlock.AppLockActivity'],
+        },
+      ],
+    },
+    {
+      key: 2,
+      name: '请用密码或指纹解锁',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 20000,
+      resetMatch: 'activity',
+      rules: [
+        {
+          action: 'back',
+          actionDelay: 16000,
+          matches: ['@[text="请用密码或指纹解锁"] - [vid="app_icon"]'],
+          activityIds: ['com.miui.applicationlock.ConfirmAccessControl'],
+        },
+      ],
+    },
+    {
+      key: 3,
+      name: '使用密码验证',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 20000,
+      resetMatch: 'activity',
+      rules: [
+        {
+          action: 'back',
+          actionDelay: 16000,
+          matches: ['@[text="使用密码验证"] <<n * [vid="icon1"]'],
+          activityIds: ['com.miui.applicationlock.ConfirmAccessControl'],
+        },
+      ],
+    },
+  ],
+},
+);
