157c4d3bcb51dd95a51832406f46652b3508b485
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2026-07-07 10:01:34 +0800

    Update com.ss.android.ugc.aweme.ts

--- src/apps/com.ss.android.ugc.aweme.ts
+++ src/apps/com.ss.android.ugc.aweme.ts
@@ -4,9 +4,25 @@ export default defineGkdApp({
   id: 'com.ss.android.ugc.aweme',
   name: '抖音',
   groups: [
-    //发财金
     {
       key: 0,
+      name: '浏览好物-返回领取',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 40000,
+      resetMatch: 'activity',
+      rules: [
+        {
+          action: 'back',
+          actionDelay: 30000,
+          matches: ['@[text="返回领取"] <<n * [id="recharge-pendant"]'],
+          activityIds: ['.live.LiveDummyActivity'],
+        },
+      ],
+    },
+    //发财金
+    {
+      key: 1,
       name: '发财金已到账-<',
       matchRoot: true,
       actionMaximum: 1,
@@ -18,6 +34,37 @@ export default defineGkdApp({
           actionDelay: 20000,
           matches: ['@[desc="券红包"] <<n * [vid="buu"]'],
           activityIds: ['.live.LiveDummyActivity'],
+        },
+      ],
+    },
+    {
+      key: 20,
+      name: '看视频-返回领取',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 40000,
+      resetMatch: 'activity',
+      rules: [
+        {
+          actionDelay: 30000,
+          excludeMatches: ['ImageView - @ImageView < ViewGroup -3 ScrollView'],
+          matches: ['@ImageView < ViewGroup -3 ScrollView'],
+          activityIds: ['com.ss.android.excitingvideo.ExcitingVideoActivity'],
+        },
+      ],
+    },
+    {
+      key: 21,
+      name: '看视频-任务完成 返回领取',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 40000,
+      resetMatch: 'activity',
+      rules: [
+        {
+          actionDelay: 30000,
+          matches: ['@ImageView < [desc^="任务完成 返回领取"]'],
+          activityIds: ['com.ss.android.excitingvideo.ExcitingVideoActivity'],
         },
       ],
     },
