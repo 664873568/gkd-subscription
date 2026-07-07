311413b83cb03e1d9ae52bb7ebf26ab79a9859b0
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2026-07-08 05:10:01 +0800

    Update matches and activityIds in kiwi.ts

---
+++ src/apps/com.duowan.kiwi.ts
@@ -0,0 +1,23 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'com.duowan.kiwi',
+  name: '虎牙直播',
+  groups: [
+    //首页广告类
+    {
+      key: 50,
+      name: '首页广告-跳过',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'app',
+      rules: [
+        {
+          matches: ['@[text="跳过"] <<n * [text="广告"]'],
+          activityIds: ['.homepage.Homepage'],
+        },
+      ],
+    },
+  ],
+});
