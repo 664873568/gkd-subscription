b1ed424d356152aa88eb9f595a9d985bacb569e9
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2026-07-07 08:58:59 +0800

    Create com.dianping.v1.ts

---
+++ src/apps/com.dianping.v1.ts
@@ -0,0 +1,24 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'com.dianping.v1',
+  name: '大众点评',
+  groups: [
+    {
+      key: 0,
+      name: '返回寻宝',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'activity',
+      rules: [
+        {
+          action: 'back',
+          actionDelay: 5000,
+          matches: ['@ImageView[clickable=false] - ImageView[clickable=false] < FrameLayout[clickable=false] < FrameLayout[clickable=true]'],
+          activityIds: ['com.dianping.shopshell.PexusPoiActivity'],
+        },
+      ],
+    },
+  ],
+});
