f79cc87e73dbdab25a7d8edd2dbfe517a529f62d
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2026-07-07 10:13:36 +0800

    Update com.sankuai.meituan.ts

--- src/apps/com.sankuai.meituan.ts
+++ src/apps/com.sankuai.meituan.ts
@@ -4,6 +4,64 @@ export default defineGkdApp({
   id: 'com.sankuai.meituan',
   name: '美团',
   groups: [
+    //看视频
+    {
+      key: 10,
+      name: '看视频-恭喜获得奖励*×',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'activity',
+      rules: [
+        {
+          matches: ['@ImageView <<n * [text="恭喜获得奖励"]'],
+          activityIds: ['com.qq.e.ads.PortraitADActivity'],
+        },
+      ],
+    },
+    {
+      key: 11,
+      name: '看视频-去看看',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'activity',
+      rules: [
+        {
+          matches: ['@[text="去看看"] <<n * [text="广告"]'],
+          activityIds: ['com.meituan.android.mrn.container.MRNStandardActivity'],
+        },
+      ],
+    },
+    {
+      key: 12,
+      name: '看视频-点击广告得',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'activity',
+      rules: [
+        {
+          matches: ['@[text="立即打开"] <<n * [text^="点击广告得"]'],
+          activityIds: ['.msv.page.activity.MSVPageActivity'],
+        },
+      ],
+    },
+    {
+      key: 13,
+      name: '看视频-<+×',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 15000,
+      resetMatch: 'activity',
+      rules: [
+        {
+          actionDelay: 10000,
+          matches: ['@ImageView + ImageView + TextView'],
+          activityIds: ['com.ubix.ssp.open.comm.UBiXWebViewActivity'],
+        },
+      ],
+    },
     //首页广告类
     {
       key: 50,
