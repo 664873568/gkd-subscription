ce6ca051c306a2baafaf2462637aac657b6590ac
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2026-07-08 06:29:01 +0800

    Increase actionDelay for back actions

--- src/apps/com.ss.android.ugc.aweme.lite.ts
+++ src/apps/com.ss.android.ugc.aweme.lite.ts
@@ -15,7 +15,7 @@ export default defineGkdApp({
       rules: [
         {
           action: 'back',
-          actionDelay: 20000,
+          actionDelay: 21000,
           matches: ['@[text="返回按钮"] <<n * [vid="ao7"]'],
           activityIds: [
             'com.ss.android.ugc.aweme.bullet.ui.BulletContainerActivity',
@@ -33,7 +33,6 @@ export default defineGkdApp({
       rules: [
         {
           action: 'back',
-          actionDelay: 16000,
           matches: [
             '@[text="a03431aadab482a12d9056e814f367cb.png~tplv-49obo7mizy-png75"] <<n * [vid="ao7"]',
           ],
