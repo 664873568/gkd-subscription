bd6cd4c7a038c91dbf61070be5c22aba5215245d
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2026-07-08 06:26:34 +0800

    Create com.xunlei.downloadprovider.ts

---
+++ src/apps/com.xunlei.downloadprovider.ts
@@ -0,0 +1,22 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'com.xunlei.downloadprovider',
+  name: '迅雷',
+  groups: [
+    {
+      key: 0,
+      name: '首页广告-×',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 40000,
+      resetMatch: 'activity',
+      rules: [
+        {
+          matches: ['@[id$="noah_interstitial_close_button"] <<n [id$="noah_interstitial_media_view"]'],
+          activityIds: ['com.noah.adn.huichuan.view.HCCommonActivity'],
+        },
+      ],
+    },
+  ],
+});
