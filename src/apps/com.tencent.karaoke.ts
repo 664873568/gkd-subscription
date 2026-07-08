625879605fc801f9b82c2fa9d251df3f7d62159a
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2026-07-08 16:00:00 +0800

    Create com.tencent.karaoke.ts

---
+++ src/apps/com.tencent.karaoke.ts
@@ -0,0 +1,23 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'com.tencent.karaoke',
+  name: '全民K歌',
+  groups: [
+    //首页广告类
+    {
+      key: 50,
+      name: '首页广告-跳过',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'app',
+      rules: [
+        {
+          matches: ['@[text="跳过"][vid="njn"] <<n * [desc="广告"][vid="o3b"]'],
+          activityIds: ['.module.splash.ui.SplashBaseActivity'],
+        },
+      ],
+    },
+  ],
+});
