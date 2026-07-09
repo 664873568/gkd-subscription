2a166eff7a719a07feebe8538dd6c9287b0c8a29
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2026-07-09 08:56:05 +0800

    Update com.sina.weibo.ts

--- src/apps/com.sina.weibo.ts
+++ src/apps/com.sina.weibo.ts
@@ -6,7 +6,22 @@ export default defineGkdApp({
   groups: [
     {
       key: 0,
-      name: '首页-跳过-广告',
+      name: '任务完成 返回YY',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'app',
+      rules: [
+        {
+          matches: ['@[text="任务完成\n返回YY"] <<n [text="用户任务中心"]'],
+          activityIds: ['.browser.WeiboBrowser'],
+        },
+      ],
+    },
+    //首页广告类
+    {
+      key: 50,
+      name: '首页广告-跳过',
       matchRoot: true,
       actionMaximum: 1,
       matchTime: 10000,
