85a8f1595b304cd830b6a1c9b5c9f2e64f59f654
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2026-07-09 10:00:05 +0800

    Update com.baidu.youavideo.ts

--- src/apps/com.baidu.youavideo.ts
+++ src/apps/com.baidu.youavideo.ts
@@ -536,7 +536,7 @@ export default defineGkdApp({
       key: 50,
       name: '首页广告-跳过',
       matchRoot: true,
-      actionMaximum: 1,
+      actionMaximum: 2,
       matchTime: 10000,
       resetMatch: 'app',
       rules: [
