614399c5b3c535b5f9fb5914cef683ab2972ab51
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2026-07-09 11:27:06 +0800

    Update com.ss.android.ugc.aweme.ts

--- src/apps/com.ss.android.ugc.aweme.ts
+++ src/apps/com.ss.android.ugc.aweme.ts
@@ -78,13 +78,15 @@ export default defineGkdApp({
     },
     {
       key: 4,
-      name: '关注订阅-×',
+      name: '抖音月付-<',
       matchRoot: true,
       actionMaximum: 1,
       matchTime: 10000,
       resetMatch: 'activity',
       rules: [
         {
+          action: 'back',
+          actionDelay: 5000,
           matches: ['@[desc="返回"] <<n * [text="抖音月付"]'],
           activityIds: ['.bullet.ui.BulletContainerActivity'],
         },
@@ -99,6 +101,7 @@ export default defineGkdApp({
       resetMatch: 'activity',
       rules: [
         {
+          actionDelay: 10000,
           matches: ['@[desc="关闭"][vid="axq"] <<n * [desc="游戏中心"][vid="b8+"]'],
           activityIds: ['com.minigame.merge.miniapphost.placeholder.MiniGameActivity0'],
         },
@@ -261,14 +264,14 @@ export default defineGkdApp({
       rules: [
         {
           action: 'back',
-          matches: ['@ImageButton <<n * [text$="tplv-49obo7mizy-75compress"]'],
+          matches: ['@ImageButton <<n * [text="01a5e1e730630a185cbd7e5910f29b68.png~tplv-49obo7mizy-75compress"]'],
           activityIds: ['.bullet.ui.BulletContainerActivity'],
         },
       ],
     },
     {
       key: 23,
-      name: '我的保障-完善健康档案',
+      name: '我的保障-<',
       matchRoot: true,
       actionMaximum: 1,
       matchTime: 10000,
@@ -276,15 +279,14 @@ export default defineGkdApp({
       rules: [
         {
           action: 'back',
-          actionDelay: 5000,
-          matches: ['@Image <<n * [desc="完善健康档案"]'],
+          matches: ['@[desc="返回"][vid="ed-"] <<n * [text="我的保障"][vid="title"]'],
           activityIds: ['.bullet.ui.BulletContainerActivity'],
         },
       ],
     },
     {
       key: 24,
-      name: '我的保障-完善健康档案-×',
+      name: '我的保障-<-广告-×',
       matchRoot: true,
       actionMaximum: 1,
       matchTime: 10000,
@@ -293,15 +295,14 @@ export default defineGkdApp({
         {
           key: 0,
           action: 'back',
-          matches: ['@ImageButton <<n * [text$="tplv-49obo7mizy-75compress"]'],
+          matches: ['@ImageButton <<n * [text="01a5e1e730630a185cbd7e5910f29b68.png~tplv-49obo7mizy-75compress"]'],
           activityIds: ['.bullet.ui.BulletContainerActivity'],
         },
         {
           preKeys: [0],
           key: 1,
           action: 'back',
-          actionDelay: 5000,
-          matches: ['@Image <<n * [desc="完善健康档案"]'],
+          matches: ['@[desc="返回"][vid="ed-"] <<n * [text="我的保障"][vid="title"]'],
           activityIds: ['.bullet.ui.BulletContainerActivity'],
         },
       ],
@@ -441,7 +442,7 @@ export default defineGkdApp({
       resetMatch: 'activity',
       rules: [
         {
-          matches: ['@ImageView < ViewGroup[index=3]'],
+          matches: ['@ImageView[index=0][childCount=0] < ViewGroup[index=3][childCount=6]'],
           activityIds: ['.main.MainActivity'],
         },
       ],
