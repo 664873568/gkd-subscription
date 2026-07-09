18ecdf8f391fbea993910561c13a7d1a44432d88
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2026-07-09 09:01:18 +0800

    Create com.miui.systemAdSolution.ts

---
+++ src/apps/com.miui.systemAdSolution.ts
@@ -0,0 +1,23 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'com.miui.systemAdSolution',
+  name: '智能服务',
+  groups: [
+    //首页广告类
+    {
+      key: 50,
+      name: '首页广告-跳过',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'app',
+      rules: [
+        {
+          matches: ['@[text^="跳过"][vid="view_skip_button"] <<n * [vid="view_slogan"]'],
+          activityIds: ['null'],
+        },
+      ],
+    },
+  ],
+});
