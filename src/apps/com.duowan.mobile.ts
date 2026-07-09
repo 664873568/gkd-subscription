0eb77e2db6cd4a39fbd9b9fe4332224a3b602cfc
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2026-07-09 13:15:48 +0800

    Update com.duowan.mobile.ts

--- src/apps/com.duowan.mobile.ts
+++ src/apps/com.duowan.mobile.ts
@@ -4,8 +4,73 @@ export default defineGkdApp({
   id: 'com.duowan.mobile',
   name: 'YY',
   groups: [
+    //做任务赢金币
     {
       key: 0,
+      name: '打开APP',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'activity',
+      rules: [
+        {
+          matches: ['@[text="打开"][vid="btn_ok"] <<n * [text="提醒"][vid="message"]'],
+          activityIds: ['com.yy.mobile.ui.common.JsSupportWebAcitivity'],
+        },
+      ],
+    },
+    {
+      key: 1,
+      name: '做任务赢金币-领奖励',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'activity',
+      rules: [
+        {
+          matches: ['@[text="领奖励"] <<n [text="做任务赢金币"]'],
+          activityIds: ['com.yy.mobile.ui.common.JsSupportWebAcitivity'],
+        },
+      ],
+    },
+    {
+      key: 2,
+      name: '做任务赢金币-恭喜获得-×',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'activity',
+      rules: [
+        {
+          matches: ['@TextView  - [text="马上完成"] -n [text="恭喜获得"]'],
+          activityIds: ['com.yy.mobile.ui.common.JsSupportWebAcitivity'],
+        },
+      ],
+    },
+    {
+      key: 3,
+      name: '做任务赢金币-领奖励-×',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'activity',
+      rules: [
+        {
+          key: 0,
+          matches: ['@[text="领奖励"] <<n [text="做任务赢金币"]'],
+          activityIds: ['com.yy.mobile.ui.common.JsSupportWebAcitivity'],
+        },
+        {
+          preKeys: [0],
+          key: 1,
+          matches: ['@TextView  - [text="马上完成"] -n [text="恭喜获得"]'],
+          activityIds: ['com.yy.mobile.ui.common.JsSupportWebAcitivity'],
+        },
+      ],
+    },
+    //功能类
+    {
+      key: 50,
       name: '打开通知权限-×',
       matchRoot: true,
       actionMaximum: 1,
@@ -19,7 +84,7 @@ export default defineGkdApp({
       ],
     },
     {
-      key: 1,
+      key: 51,
       name: '发现新版本-立即下载-×',
       matchRoot: true,
       actionMaximum: 1,
@@ -27,7 +92,7 @@ export default defineGkdApp({
       resetMatch: 'activity',
       rules: [
         {
-          matches: ['@[vid="btn_cancel"] <<n * [vid="btn_ok"]'],
+          matches: ['@[vid="btn_cancel"] <<n [vid="btn_ok"]'],
           activityIds: ['com.yy.mobile.ui.common.JsSupportWebAcitivity'],
         },
       ],
