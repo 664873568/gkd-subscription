912ae6ec73247d9411ac6602eb3d704618486fa5
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2026-07-10 10:57:32 +0800

    Update com.miui.securitycenter.ts

--- src/apps/com.miui.securitycenter.ts
+++ src/apps/com.miui.securitycenter.ts
@@ -33,7 +33,7 @@ export default defineGkdApp({
         {
           action: 'back',
           matches: [
-            '@[text="启动应用"][vid="permission_group_title"] + [text="京东金融 想要打开 京东，是否允许？"][vid="event_title"]',
+            '@[text="启动应用"][vid="permission_group_title"] + [text=^"京东金融"][vid="event_title"]',
           ],
           activityIds: ['com.miui.wakepath.ui.ConfirmStartActivity', 'null'],
         },
