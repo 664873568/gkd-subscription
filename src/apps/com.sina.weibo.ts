928826ec8e83798ad44ca3f903d65351ad6a3a66
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2026-07-10 10:09:36 +0800

    Update com.sina.weibo.ts

--- src/apps/com.sina.weibo.ts
+++ src/apps/com.sina.weibo.ts
@@ -6,6 +6,20 @@ export default defineGkdApp({
   groups: [
     {
       key: 0,
+      name: '连续签到-×',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'app',
+      rules: [
+        {
+          matches: ['@TextView - CheckBox - [text^="连续签到"]'],
+          activityIds: ['.browser.WeiboBrowser'],
+        },
+      ],
+    },
+    {
+      key: 1,
       name: '任务完成 返回YY',
       matchRoot: true,
       actionMaximum: 1,
@@ -13,6 +27,27 @@ export default defineGkdApp({
       resetMatch: 'app',
       rules: [
         {
+          matches: ['@[text="任务完成\\n返回YY"] <<n [text="用户任务中心"]'],
+          activityIds: ['.browser.WeiboBrowser'],
+        },
+      ],
+    },
+    {
+      key: 2,
+      name: '任务完成 返回YY-连续签到-×',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'app',
+      rules: [
+        {
+          key: 0,
+          matches: ['@TextView - CheckBox - [text^="连续签到"]'],
+          activityIds: ['.browser.WeiboBrowser'],
+        },
+        {
+          preKeys: [0],
+          key: 1,
           matches: ['@[text="任务完成\\n返回YY"] <<n [text="用户任务中心"]'],
           activityIds: ['.browser.WeiboBrowser'],
         },
