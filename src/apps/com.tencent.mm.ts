52964b4c6d7e610dad9bb9ffbef56434dd850e43
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2026-07-13 13:15:56 +0800

    Update com.tencent.mm.ts

--- src/apps/com.tencent.mm.ts
+++ src/apps/com.tencent.mm.ts
@@ -6,24 +6,17 @@ export default defineGkdApp({
   groups: [
     {
       key: 0,
-      name: '领取云添加次数',
+      name: '看广告-领取',
       matchRoot: true,
       actionMaximum: 1,
-      matchTime: 35000,
+      matchTime: 1000,
       resetMatch: 'activity',
       rules: [
         {
-          key: 0,
+          excludeMatches: ['@[text="关闭"] <<n * [text="已获得奖励"]'],
           matches: [
             '@[text="领取"] <<n * [text="看广告免费领取迅雷云添加次数"]',
           ],
-          activityIds: ['.plugin.appbrand.ui.AppBrandUI00'],
-        },
-        {
-          preKeys: 0,
-          key: 1,
-          actionDelay: 30000,
-          matches: ['@[text="关闭"] <<n * [text="已获得奖励"]'],
           activityIds: ['.plugin.appbrand.ui.AppBrandUI00'],
         },
       ],
@@ -39,6 +32,56 @@ export default defineGkdApp({
         {
           actionDelay: 30000,
           matches: ['@[text="关闭"] <<n * [text="已获得奖励"]'],
+          activityIds: ['.plugin.appbrand.ui.AppBrandUI00'],
+        },
+      ],
+    },
+    {
+      key: 2,
+      name: '看广告-已获得奖励',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 35000,
+      resetMatch: 'activity',
+      rules: [
+        {
+          key: 0,
+          excludeMatches: ['@[text="关闭"] <<n * [text="已获得奖励"]'],
+          matches: [
+            '@[text="领取"] <<n * [text="看广告免费领取迅雷云添加次数"]',
+          ],
+          activityIds: ['.plugin.appbrand.ui.AppBrandUI00'],
+        },
+        {
+          preKeys: 0,
+          key: 1,
+          actionDelay: 30000,
+          matches: ['@[text="关闭"] <<n * [text="已获得奖励"]'],
+          activityIds: ['.plugin.appbrand.ui.AppBrandUI00'],
+        },
+      ],
+    },
+    {
+      key: 3,
+      name: '已获得奖励-看广告',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 35000,
+      resetMatch: 'activity',
+      rules: [
+        {
+          key: 0,
+          actionDelay: 30000,
+          matches: ['@[text="关闭"] <<n * [text="已获得奖励"]'],
+          activityIds: ['.plugin.appbrand.ui.AppBrandUI00'],
+        },
+        {
+          preKeys: 0,
+          key: 1,
+          excludeMatches: ['@[text="关闭"] <<n * [text="已获得奖励"]'],
+          matches: [
+            '@[text="领取"] <<n * [text="看广告免费领取迅雷云添加次数"]',
+          ],
           activityIds: ['.plugin.appbrand.ui.AppBrandUI00'],
         },
       ],
