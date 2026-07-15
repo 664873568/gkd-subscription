45ef4076444042b8c24b4403b104c60d3d78d7a4
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2026-07-15 13:15:31 +0800

    Update com.jingdong.app.mall.ts

--- src/apps/com.jingdong.app.mall.ts
+++ src/apps/com.jingdong.app.mall.ts
@@ -81,5 +81,20 @@ export default defineGkdApp({
         },
       ],
     },
+    //首页广告类
+    {
+      key: 50,
+      name: '未成年模式-我知道了',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'app',
+      rules: [
+        {
+          matches: ['@[text="跳过"] < [desc="跳过"][vid="splash_skip_container"] -2 [desc="启动图广告"]'],
+          activityIds: ['.MainFrameActivity'],
+        },
+      ],
+    },
   ],
 });
