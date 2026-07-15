db7fd7b87f3691accbf0861dd6e6f969d07ad1f2
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2026-07-15 12:45:42 +0800

    Update com.xiaomi.market.ts

--- src/apps/com.xiaomi.market.ts
+++ src/apps/com.xiaomi.market.ts
@@ -6,7 +6,7 @@ export default defineGkdApp({
   groups: [
     {
       key: 0,
-      name: '应用商店-小米应用商店-关闭',
+      name: '小米应用商店-关闭',
       matchRoot: true,
       actionMaximum: 1,
       matchTime: 20000,
@@ -17,6 +17,21 @@ export default defineGkdApp({
           actionDelay: 16000,
           anyMatches: ['@[desc="关闭"] <<n * [text="小米应用商店"]'],
           activityIds: ['null'],
+        },
+      ],
+    },
+    //首页广告类
+    {
+      key: 50,
+      name: '首页广告-关闭',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'activity',
+      rules: [
+        {
+          anyMatches: ['@[desc="关闭"][vid="iv_close"]'],
+          activityIds: ['.business_ui.main.MarketTabActivity'],
         },
       ],
     },
