ba522f86858b767ebc9388b7f5384fe1b59b5e75
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2026-07-15 12:17:30 +0800

    Update com.dianping.v1.ts

--- src/apps/com.dianping.v1.ts
+++ src/apps/com.dianping.v1.ts
@@ -22,16 +22,15 @@ export default defineGkdApp({
     },
     {
       key: 1,
-      name: '恭喜获得-返回寻宝',
+      name: '返回寻宝',
       matchRoot: true,
       actionMaximum: 1,
       matchTime: 10000,
       resetMatch: 'activity',
       rules: [
         {
-          action: 'back',
           matches: [
-            '@ImageView[index=1][childCount=0] - ImageView[index=0][childCount=0] <<n * [text="恭喜获得"]',
+            '@ImageView[index=1][childCount=0] - ImageView[index=0][childCount=0] <<4 FrameLayout',
           ],
           activityIds: [
             'com.dianping.shopshell.PexusPoiActivity',
@@ -42,6 +41,22 @@ export default defineGkdApp({
     },
     {
       key: 2,
+      name: '浏览完成',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'activity',
+      rules: [
+        {
+          matches: [
+            '@[text="浏览完成"] - ImageView - ImageView',
+          ],
+          activityIds: ['com.dianping.nova.picasso.DPPicassoBoxActivity'],
+        },
+      ],
+    },
+    {
+      key: 3,
       name: '继续开宝箱集卡',
       matchRoot: true,
       actionMaximum: 1,
@@ -57,7 +72,7 @@ export default defineGkdApp({
       ],
     },
     {
-      key: 3,
+      key: 4,
       name: '查看附近的店',
       matchRoot: true,
       actionMaximum: 1,
@@ -65,7 +80,6 @@ export default defineGkdApp({
       resetMatch: 'activity',
       rules: [
         {
-          actionDelay: 1000,
           matches: [
             '@ImageView[clickable=true] < FrameLayout - FrameLayout > ImageView[clickable=true]',
           ],
