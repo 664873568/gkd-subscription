b8532c05329396c9d2b7ee69d2974bfd7c9256ea
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2026-07-18 10:40:15 +0800

    Update advertisement names and rules in systemAdSolution

--- src/apps/com.miui.systemAdSolution.ts
+++ src/apps/com.miui.systemAdSolution.ts
@@ -2,12 +2,12 @@ import { defineGkdApp } from '@gkd-kit/define';
 
 export default defineGkdApp({
   id: 'com.miui.systemAdSolution',
-  name: '智能服务',
+  name: '智能服务/',
   groups: [
     //首页广告类
     {
       key: 50,
-      name: '首页广告-跳过',
+      name: '首页-跳过广告',
       matchRoot: true,
       actionMaximum: 1,
       matchTime: 10000,
@@ -15,7 +15,7 @@ export default defineGkdApp({
       rules: [
         {
           matches: [
-            '@[text^="跳过"][vid="view_skip_button"] <<n * [text="小米浏览器"][vid="view_app_name"]',
+            '@[text^="跳过广告"][vid="view_skip_button"] <<n [vid="view_splashAdSloganFlagView"]',
           ],
           activityIds: ['null'],
         },
