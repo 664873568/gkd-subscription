a0ebf77743f15cc40edaeb64b48d07fba09ab09d
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2026-07-18 10:27:33 +0800

    Update com.taobao.taobao.ts

--- src/apps/com.taobao.taobao.ts
+++ src/apps/com.taobao.taobao.ts
@@ -118,5 +118,20 @@ export default defineGkdApp({
         },
       ],
     },
+    //首页广告类
+    {
+      key: 50,
+      name: '首页广告-跳过',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'app',
+      rules: [
+        {
+          matches: ['@[desc="关闭按钮"] - [vid="poplayer_native_state_id"]'],
+          activityIds: ['com.taobao.tao.welcome.Welcome'],
+        },
+      ],
+    },
   ],
 });
