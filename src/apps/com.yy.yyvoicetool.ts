7860eabe5889ab98edcee34139f8bfd3e0fb6eec
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2026-07-18 13:04:10 +0800

    Update com.yy.yyvoicetool.ts

--- src/apps/com.yy.yyvoicetool.ts
+++ src/apps/com.yy.yyvoicetool.ts
@@ -174,5 +174,20 @@ export default defineGkdApp({
         },
       ],
     },
+    //首页广告类
+    {
+      key: 50,
+      name: '首页广告-跳过',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'app',
+      rules: [
+        {
+          matches: ['@[text="跳过"] + [text~="\\d"]'],
+          activityIds: ['.MainActivity'],
+        },
+      ],
+    },
   ],
 });
