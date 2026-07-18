628b87e810a18413487f7a0721b3d0bebfa640a2
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2026-07-18 13:09:27 +0800

    Create com.dz.hmjc.ts

---
+++ src/apps/com.dz.hmjc.ts
@@ -0,0 +1,23 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'com.dz.hmjc',
+  name: '河马剧场',
+  groups: [
+    //首页广告类
+    {
+      key: 50,
+      name: '首页广告-跳过',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'app',
+      rules: [
+        {
+          matches: ['@[text="跳过"] + [text~="\\d"]'],
+          activityIds: ['com.dz.business.main.ui.MainActivity'],
+        },
+      ],
+    },
+  ],
+});
