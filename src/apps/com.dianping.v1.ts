a5eb1ffac4193b40d0bed1e80ef9b70a37cb14ca
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2026-07-18 13:27:12 +0800

    Update com.dianping.v1.ts

--- src/apps/com.dianping.v1.ts
+++ src/apps/com.dianping.v1.ts
@@ -13,8 +13,18 @@ export default defineGkdApp({
       resetMatch: 'activity',
       rules: [
         {
+          key: 0,
+          actionDelay: 1000,
           matches: [
             '@ImageView < FrameLayout <n FrameLayout + ImageView[clickable=true]',
+          ],
+          activityIds: ['com.dianping.nova.picasso.DPPicassoBoxActivity'],
+        },
+        {
+          preKeys: [0],
+          key: 1,
+          matches: [
+            '@ImageView[clickable=true] - FrameLayout >2 ImageView',
           ],
           activityIds: ['com.dianping.nova.picasso.DPPicassoBoxActivity'],
         },
@@ -22,25 +32,6 @@ export default defineGkdApp({
     },
     {
       key: 1,
-      name: '返回寻宝',
-      matchRoot: true,
-      actionMaximum: 1,
-      matchTime: 15000,
-      resetMatch: 'activity',
-      rules: [
-        {
-          matches: [
-            '@ImageView[index=1][childCount=0] - ImageView[index=0][childCount=0] <<4 FrameLayout',
-          ],
-          activityIds: [
-            'com.dianping.shopshell.PexusPoiActivity',
-            'com.sankuai.waimai.business.restaurant.poicontainer.WMRestaurantActivity',
-          ],
-        },
-      ],
-    },
-    {
-      key: 2,
       name: '浏览完成',
       matchRoot: true,
       actionMaximum: 1,
@@ -48,8 +39,33 @@ export default defineGkdApp({
       resetMatch: 'activity',
       rules: [
         {
-          matches: ['@[text="浏览完成"] - ImageView - ImageView'],
-          activityIds: ['com.dianping.nova.picasso.DPPicassoBoxActivity'],
+          matches: ['@[text="浏览完成"] - ImageView - ImageView < FrameLayout'],
+          activityIds: [
+            'com.dianping.nova.picasso.DPPicassoBoxActivity',
+            'com.meituan.android.mrn.container.MRNBaseActivity',
+          ],
+        },
+      ],
+    },
+    {
+      key: 2,
+      name: '返回寻宝',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 15000,
+      resetMatch: 'activity',
+      rules: [
+        {
+          action: 'back',
+          actionDelay: 1000,
+          anyMatches: [
+            '@[text="恭喜获得"] + [text="100点金币"]',
+            '@ImageView[index=1][childCount=0] - ImageView[index=0][childCount=0] <<4 FrameLayout',
+          ],
+          activityIds: [
+            'com.dianping.shopshell.PexusPoiActivity',
+            'com.sankuai.waimai.business.restaurant.poicontainer.WMRestaurantActivity',
+          ],
         },
       ],
     },
