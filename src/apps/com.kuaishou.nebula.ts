a3565d8bf4f3f541ca83e4fc38daefa2621c8391
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2026-07-20 20:55:56 +0800

    Update com.kuaishou.nebula.ts

--- src/apps/com.kuaishou.nebula.ts
+++ src/apps/com.kuaishou.nebula.ts
@@ -5,6 +5,21 @@ export default defineGkdApp({
   name: '快手极速版',
   groups: [
     {
+      key: 0,
+      name: '打卡失败',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 20000,
+      resetMatch: 'activity',
+      rules: [
+        {
+          action: 'back',
+          matches: ['@[text="与奖品擦肩而过"] +n View [text="重新选择商品"]'],
+          activityIds: ['com.yxcorp.gifshow.webview.KwaiYodaWebViewActivity'],
+        },
+      ],
+    },
+    {
       key: 1,
       name: '点赞',
       matchRoot: true,
@@ -13,6 +28,28 @@ export default defineGkdApp({
       resetMatch: 'activity',
       rules: [
         {
+          matches: ['@[vid="like_icon"][visibleToUser=true]'],
+          activityIds: ['com.yxcorp.gifshow.HomeActivity'],
+        },
+      ],
+    },
+    {
+      key: 2,
+      name: '打卡失败-点赞',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 20000,
+      resetMatch: 'activity',
+      rules: [
+        {
+          key: 0,
+          action: 'back',
+          matches: ['@[text="与奖品擦肩而过"] +n View [text="重新选择商品"]'],
+          activityIds: ['com.yxcorp.gifshow.webview.KwaiYodaWebViewActivity'],
+        },
+        {
+          preKeys: [0],
+          key: 1,
           matches: ['@[vid="like_icon"][visibleToUser=true]'],
           activityIds: ['com.yxcorp.gifshow.HomeActivity'],
         },
