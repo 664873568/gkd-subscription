619563a58bde0d936c110bea5a482309fed1bf25
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2026-07-21 17:46:05 +0800

    Update com.dz.hmjc.ts

--- src/apps/com.dz.hmjc.ts
+++ src/apps/com.dz.hmjc.ts
@@ -4,6 +4,21 @@ export default defineGkdApp({
   id: 'com.dz.hmjc',
   name: '河马剧场',
   groups: [
+    //福利中心
+    {
+      key: 0,
+      name: '福利中心-立即签到',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'activity',
+      rules: [
+        {
+          matches: ['@[text^="立即签到"] -n [text="连续签到得金币"] - ImageButton'],
+          activityIds: ['com.dz.business.main.ui.MainActivity'],
+        },
+      ],
+    },
     //首页广告类
     {
       key: 50,
@@ -11,7 +26,7 @@ export default defineGkdApp({
       matchRoot: true,
       actionMaximum: 1,
       matchTime: 10000,
-      resetMatch: 'app',
+      resetMatch: 'activity',
       rules: [
         {
           matches: ['@[text="跳过"] + [text~="\\\\d"]'],
