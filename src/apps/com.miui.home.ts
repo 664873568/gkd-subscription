cffb62d8659b9bf0735c28310e46f5358d41e391
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2026-07-27 18:28:52 +0800

    Update com.miui.home.ts

--- src/apps/com.miui.home.ts
+++ src/apps/com.miui.home.ts
@@ -13,7 +13,7 @@ export default defineGkdApp({
       resetMatch: 'app',
       rules: [
         {
-          actinon: 'clickCenter',
+          action: 'clickCenter',
           actionDelay: 16000,
           anyMatches: [
             '@[desc^="YY"] >n [desc^="打开"][vid="icon"]',
