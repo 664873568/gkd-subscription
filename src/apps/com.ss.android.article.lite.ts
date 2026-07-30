59c66a140855d47b466a045c4b78eedd67a094d4
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2026-07-30 19:35:08 +0800

    Create com.ss.android.article.lite.ts

---
+++ src/apps/com.ss.android.article.lite.ts
@@ -0,0 +1,28 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'com.ss.android.article.lite',
+  name: '今日头条极速版',
+  groups: [
+    {
+      key: 0,
+      name: 'YY-去头条极速版',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'activity',
+      rules: [
+        {
+          action: 'back',
+          actionDelay: 5000,
+          matches: [
+            '[text="搜你想看的"][desc="搜索框，搜你想看的"] < * - @[desc="返回"] <<n * - * [text="点击赚钱"]',
+          ],
+          activityIds: [
+            'com.ss.android.ugc.detail.activity.TikTokActivity',
+          ],
+        },
+      ],
+    },
+  ],
+});
