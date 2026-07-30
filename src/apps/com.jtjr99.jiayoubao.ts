27d04789785a0f37a6a050ca05bf955f222e8b2a
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2026-07-30 19:19:39 +0800

    Update com.jtjr99.jiayoubao.ts

--- src/apps/com.jtjr99.jiayoubao.ts
+++ src/apps/com.jtjr99.jiayoubao.ts
@@ -7,14 +7,28 @@ export default defineGkdApp({
     //首页广告类
     {
       key: 50,
-      name: '首页-跳过',
+      name: '首页广告-×',
       matchRoot: true,
       actionMaximum: 1,
       matchTime: 10000,
       resetMatch: 'app',
       rules: [
         {
-          matches: ['@[text^="跳过"][vid="btn_skip"]'],
+          matches: ['@[vid="iv_close"][clickable=true]'],
+          activityIds: ['.module.home.MainTabActivity'],
+        },
+      ],
+    },
+    {
+      key: 51,
+      name: '广告-×',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'app',
+      rules: [
+        {
+          matches: ['@[vid="iv_close"][clickable=true]'],
           activityIds: ['.module.ucenter.enter.SplashScreenActivity'],
         },
       ],
