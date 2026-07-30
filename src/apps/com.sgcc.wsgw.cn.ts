74ed730a2d3f138bbfbd4bbc6ccad9b831300ee1
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2026-07-30 19:44:02 +0800

    Update com.sgcc.wsgw.cn.ts

--- src/apps/com.sgcc.wsgw.cn.ts
+++ src/apps/com.sgcc.wsgw.cn.ts
@@ -4,6 +4,20 @@ export default defineGkdApp({
   id: 'com.sgcc.wsgw.cn',
   name: '网上国网',
   groups: [
+    {
+      key: 0,
+      name: '签到成功-×',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'activity',
+      rules: [
+        {
+          matches: ['ImageButton < View - @TextView[text=""][clickable=true] -2 [text="签到成功"]'],
+          activityIds: ['com.sgcc.wsgw.mainbundle.ElectricTitleActivity'],
+        },
+      ],
+    },
     //首页广告类
     {
       key: 50,
