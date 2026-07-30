993f09f2ed7850616cbf4635137b623fb6452488
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2026-07-30 19:12:24 +0800

    Update com.ss.android.ugc.aweme.ts

--- src/apps/com.ss.android.ugc.aweme.ts
+++ src/apps/com.ss.android.ugc.aweme.ts
@@ -303,10 +303,7 @@ export default defineGkdApp({
       rules: [
         {
           anyMatches: [
-            '@Button[desc="关闭"] - View > [text="7711e33a3ceb4b608189a89f05469b9f~tplv-20ashz96qn-1"]',
-            '@Button[desc="关闭"] - View > [text="87300a5123d94e85a80a88a1ad3afc38~tplv-20ashz96qn-1"]',
-            '@Button[desc="关闭"] - View > [text="9ce5c5471aae4dcfa14d3bbf983c3368~tplv-20ashz96qn-1"]',
-            '@Button[desc="关闭"] - View > [text="ce22a8e2980d4d6a8a6562c3cd130b22~tplv-20ashz96qn-1"]',
+            '@Button[desc="关闭"][clickable=true] - View[text=""][clickable=true] > [text$="~tplv-20ashz96qn-1"]',
             '@[desc="残忍离开"] < ViewGroup -n [desc="你有信用卡还款金未使用，确认离开吗"]',
           ],
           activityIds: ['.live.LiveDummyActivity'],
@@ -362,11 +359,8 @@ export default defineGkdApp({
       rules: [
         {
           key: 0,
-          anyMatches: [
-            '@Button[desc="关闭"] - View > [text="7711e33a3ceb4b608189a89f05469b9f~tplv-20ashz96qn-1"]',
-            '@Button[desc="关闭"] - View > [text="87300a5123d94e85a80a88a1ad3afc38~tplv-20ashz96qn-1"]',
-            '@Button[desc="关闭"] - View > [text="9ce5c5471aae4dcfa14d3bbf983c3368~tplv-20ashz96qn-1"]',
-            '@Button[desc="关闭"] - View > [text="ce22a8e2980d4d6a8a6562c3cd130b22~tplv-20ashz96qn-1"]',
+          matches: [
+            '@Button[desc="关闭"][clickable=true] - View[text=""][clickable=true] > [text$="~tplv-20ashz96qn-1"]',
           ],
           activityIds: ['.live.LiveDummyActivity'],
         },
@@ -517,7 +511,6 @@ export default defineGkdApp({
       rules: [
         {
           key: 0,
-          actionDelay: 5000,
           matches: ['@[desc="返回"] + [text="生活缴费"]'],
           activityIds: [
             'com.bytedance.android.anniex.container.AnnieXHostActivity',
@@ -575,7 +568,7 @@ export default defineGkdApp({
       name: '车主服务-月付金-广告',
       matchRoot: true,
       actionMaximum: 1,
-      matchTime: 10000,
+      matchTime: 20000,
       resetMatch: 'activity',
       rules: [
         {
@@ -693,7 +686,7 @@ export default defineGkdApp({
       name: '车主服务-搜索「车险」进入频道',
       matchRoot: true,
       actionMaximum: 1,
-      matchTime: 10000,
+      matchTime: 20000,
       resetMatch: 'activity',
       rules: [
         {
