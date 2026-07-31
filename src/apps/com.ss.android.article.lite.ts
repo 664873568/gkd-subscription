2be1458d53bee23185dadeaf7c98246904bccc6c
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2026-07-31 17:51:56 +0800

    Update com.ss.android.article.lite.ts

--- src/apps/com.ss.android.article.lite.ts
+++ src/apps/com.ss.android.article.lite.ts
@@ -15,8 +15,36 @@ export default defineGkdApp({
         {
           action: 'back',
           actionDelay: 5000,
-          matches: [
+          anyMatches: [
             '[text="搜你想看的"][desc="搜索框，搜你想看的"] < * - @[desc="返回"] <<n * - * [text="点击赚钱"]',
+            '[text="搜你想看的"][desc="搜索框，搜你想看的"] < * - @[desc="返回"] <<n * - * [text="登录领钱"]',
+          ],
+          activityIds: ['com.ss.android.ugc.detail.activity.TikTokActivity'],
+        },
+      ],
+    },
+    //功能应用类
+    {
+      key: 40,
+      name: '升级版本-×',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'activity',
+      rules: [
+        {
+          key: 0,
+          matches: ['@[desc="关闭"] -n [text="升级版本"]'],
+          activityIds: ['com.ss.android.ugc.detail.activity.TikTokActivity'],
+        },
+        {
+          preKeys: [0],
+          key: 1,
+          action: 'back',
+          actionDelay: 5000,
+          anyMatches: [
+            '[text="搜你想看的"][desc="搜索框，搜你想看的"] < * - @[desc="返回"] <<n * - * [text="点击赚钱"]',
+            '[text="搜你想看的"][desc="搜索框，搜你想看的"] < * - @[desc="返回"] <<n * - * [text="登录领钱"]',
           ],
           activityIds: ['com.ss.android.ugc.detail.activity.TikTokActivity'],
         },
