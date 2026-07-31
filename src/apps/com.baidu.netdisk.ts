abb4df8f49a97bd346b446a0e5e5dd95cb97dc21
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2026-07-31 12:41:23 +0800

    Update com.baidu.netdisk.ts

--- src/apps/com.baidu.netdisk.ts
+++ src/apps/com.baidu.netdisk.ts
@@ -98,6 +98,7 @@ export default defineGkdApp({
       resetMatch: 'activity',
       rules: [
         {
+          actionDelay: 2000,
           excludeMatches: ['[text="svg%3e"] + [text~="再逛[0-9]+秒后可领奖"]'],
           matches: [
             '@RelativeLayout[clickable=true] <<n FrameLayout + FrameLayout + FrameLayout',
