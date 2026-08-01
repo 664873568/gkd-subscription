ea5ba9b079f1cf78df231807c5470fb77150e939
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2026-08-01 12:23:00 +0800

    Update com.tencent.mtt.ts

--- src/apps/com.tencent.mtt.ts
+++ src/apps/com.tencent.mtt.ts
@@ -4,6 +4,22 @@ export default defineGkdApp({
   id: 'com.tencent.mtt',
   name: 'QQ浏览器',
   groups: [
+    //功能应用类
+    {
+      key: 40,
+      name: '首页广告-跳过',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'app',
+      rules: [
+        {
+          matches: [
+            '@[text="以后再说"][vid="qb_dialog_second_button"] - [text="立即更新"][vid="qb_dialog_first_button"] -2 [text="新版发布"][vid="qb_dialog_title_view"]',
+          ],
+        },
+      ],
+    },
     //首页广告类
     {
       key: 50,
@@ -15,7 +31,6 @@ export default defineGkdApp({
       rules: [
         {
           matches: ['@View + ImageView + FrameLayout >n [vid="contentTv"]'],
-          activityIds: ['null'],
         },
       ],
     },
