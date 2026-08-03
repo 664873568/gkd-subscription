dcea52bd87a1c7f296fcd2da3e77d4760318172e
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2026-08-03 14:08:33 +0800

    Update match rules for ImageView in com.tencent.mtt.ts

--- src/apps/com.tencent.mtt.ts
+++ src/apps/com.tencent.mtt.ts
@@ -30,8 +30,8 @@ export default defineGkdApp({
       resetMatch: 'app',
       rules: [
         {
-          matches: [
-            '@View + ImageView + FrameLayout >n [vid="contentTv"]',
+          anyMatches: [
+            '@View[clickable=true] + ImageView + FrameLayout >n [vid="contentTv"]',
             '[text~="[0-9]"] - [text="跳过"] < @LinearLayout[clickable=true]',
           ],
         },
