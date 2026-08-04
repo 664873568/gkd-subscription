dc69236674033bc7d5e63829378ced8c27cce5f3
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2026-08-04 14:01:11 +0800

    Update com.miui.home.ts

--- src/apps/com.miui.home.ts
+++ src/apps/com.miui.home.ts
@@ -9,6 +9,7 @@ export default defineGkdApp({
       name: '系统桌面-最近任务-启动应用',
       matchRoot: true,
       actionMaximum: 1,
+      forcedTime: 20000,
       matchTime: 20000,
       resetMatch: 'app',
       rules: [
