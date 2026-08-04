bca88e224c0a8ba018e2413a17f0a84a7bebb699
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2026-08-04 19:14:38 +0800

    Update com.baidu.netdisk.ts

--- src/apps/com.baidu.netdisk.ts
+++ src/apps/com.baidu.netdisk.ts
@@ -1184,6 +1184,9 @@ export default defineGkdApp({
         {
           key: 0,
           actionDelay: 2000,
+          excludeMatches: [
+            '@[text="上传中..."] - View < View < View',
+          ],
           matches: [
             '@[text=""][clickable=true] + [id="noticeSwiper"] <<n * + * [text="上传任意文件"]',
           ],
