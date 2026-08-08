9802b28834ca3a13266ccefc1979c4b1c9480f69
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2026-08-08 20:53:54 +0800

    Update com.unionpay.ts

--- src/apps/com.unionpay.ts
+++ src/apps/com.unionpay.ts
@@ -23,5 +23,52 @@ export default defineGkdApp({
         },
       ],
     },
+    {
+      key: 1,
+      name: '玩转中心-去抽奖',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'activity',
+      rules: [
+        {
+          key: 0,
+          matches: [
+            'Image + * > @[text="去抽奖"][clickable=true] + TextView[clickable=true]',
+          ],
+          activityIds: [
+            '.liteapp.app.UPLiteAppActivity2',
+          ],
+        },
+        {
+          preKeys: [0],
+          key: 1,
+          matches: [
+            '[id="root-router-view"] @[text~="剩余 [1-9]次 抽奖机会"][clickable=true]',
+          ],
+          activityIds: [
+            '.liteapp.app.UPLiteAppActivity2',
+          ],
+        },
+      ],
+    },
+    {
+      key: 2,
+      name: '玩转中心-签到抽大奖',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'activity',
+      rules: [
+        {
+          matches: [
+            '[id="root-router-view"] @[text~="剩余 [1-9]次 抽奖机会"][clickable=true]',
+          ],
+          activityIds: [
+            '.liteapp.app.UPLiteAppActivity2',
+          ],
+        },
+      ],
+    },
   ],
 });
