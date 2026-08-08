10d383534c20c43014a8d767a764c71197a4f610
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2026-08-08 20:16:34 +0800

    Create com.suning.mobile.ebuy.ts

---
+++ src/apps/com.suning.mobile.ebuy.ts
@@ -0,0 +1,26 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'com.suning.mobile.ebuy',
+  name: '苏宁易购',
+  groups: [
+    {
+      key: 0,
+      name: '星图金融-返回',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'activity',
+      rules: [
+        {
+          action: 'back',
+          actionDelay: 5000,
+          matches: [
+            '@[desc="返回"][vid="btn_back"][clickable=true] < [vid="layout_ucwv_back"] + [text="云钻魔法狮"][vid="title"]',
+          ],
+          activityIds: ['com.suning.mobile.ucwv.ui.WebViewActivity'],
+        },
+      ],
+    },
+  ],
+});
