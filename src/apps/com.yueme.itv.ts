284e27dd27b8a583efc5f9f90293f2cdb8650c87
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2026-08-12 14:36:46 +0800

    Create com.yueme.itv.ts

---
+++ src/apps/com.yueme.itv.ts
@@ -0,0 +1,64 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'com.yueme.itv',
+  name: '小翼管家',
+  groups: [
+    //翼豆乐园
+    {
+      key: 0,
+      name: '翼豆乐园-去领取',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'app',
+      rules: [
+        {
+          matches: [
+            'View > TextView[clickable=true] + @View[clickable=true] > [text="去领取"]',
+          ],
+          activityIds: [
+            'com.yueme.itvhome.ui.activity.base.FragmentContainerActivity',
+          ],
+        },
+      ],
+    },
+    //首页广告类
+    {
+      key: 50,
+      name: '首页广告-跳过',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'app',
+      rules: [
+        {
+          matches: [
+            '[vid="rl_pangle_ad"] >n @View[clickable=true] + TextView[clickable=true]',
+          ],
+          activityIds: [
+            'com.chinatelecom.smarthome.module.mainpage.ui.CoverActivity',
+          ],
+        },
+      ],
+    },
+    {
+      key: 50,
+      name: '首页广告-×',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'app',
+      rules: [
+        {
+          matches: [
+            '@[vid="img_close"][clickable=true] - [vid="content_layout"] > [vid="adv_click"]',
+          ],
+          activityIds: [
+            'com.chinatelecom.smarthome.module.mainpage.ui.SmartHomeActivity',
+          ],
+        },
+      ],
+    },
+  ],
+});
