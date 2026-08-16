4f3c7fa0d0fc4c7a4bee20ec251ab944662156b6
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2026-08-16 17:46:04 +0800

    Update com.jifen.qukan.ts

--- src/apps/com.jifen.qukan.ts
+++ src/apps/com.jifen.qukan.ts
@@ -6,16 +6,18 @@ export default defineGkdApp({
   groups: [
     {
       key: 0,
-      name: '限时福利-自动看视频',
+      name: '限时福利-*s后自动看视频',
       matchRoot: true,
       actionMaximum: 1,
+      matchDelay: 5000,
       matchTime: 20000,
       resetMatch: 'activity',
       rules: [
         {
           matches: [
-            '@[text$="自动看视频"][vid="tv_countdown"] -n [vid="xad_dialog_image"]',
+            '[vid="iv_close"] +n @[text~="[0-9]s后自动看视频"][vid="tv_countdown"][clickable=true] + [vid="ll_bootom"] > [text="直接签到"][vid="tv_sign_derect"]',
           ],
+          activityIds: ['com.jifen.qkbase.main.MainActivity'],
         },
       ],
     },
@@ -25,14 +27,16 @@ export default defineGkdApp({
       name: '首页广告-跳过',
       matchRoot: true,
       actionMaximum: 1,
+      matchDelay: 5000,
       matchTime: 10000,
       resetMatch: 'activity',
       rules: [
         {
           anyMatches: [
-            '[vid="img_splash_ad"] >n @[desc="CLOSE_AD"][vid="fanti_ad_count_and_skip_container_ex"][clickable=true] > [vid="fanti_ad_count_and_skip_container"] > [text^="跳过"][vid="fanti_ad_txt_skip"] + [text~="[0-9]"][vid="fanti_ad_txt_count_down"]',
+            '[vid="img_splash_ad"] + @[text~="跳过 [0-9]s"][vid="tv_splash_ad_skip"][clickable=true]',
+            '[vid="img_splash_ad"] >n @[desc="CLOSE_AD"][vid="fanti_ad_count_and_skip_container_ex"][clickable=true] > [vid="fanti_ad_count_and_skip_container"] > [text="跳过"][vid="fanti_ad_txt_skip"] + [text~="[0-9]"][vid="fanti_ad_txt_count_down"]',
           ],
-          activityIds: ['null', 'com.jifen.qkbase.main.MainActivity'],
+          activityIds: ['com.jifen.qkbase.main.MainActivity',],
         },
       ],
     },
