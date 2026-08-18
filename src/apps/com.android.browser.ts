9e6800ea9e61fb82371ac411cb90dcaa56975d04
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2026-08-18 12:59:10 +0800

    Update com.android.browser.ts

--- src/apps/com.android.browser.ts
+++ src/apps/com.android.browser.ts
@@ -14,14 +14,14 @@ export default defineGkdApp({
       rules: [
         {
           anyMatches: [
-            '@[text="允许"] <<n * [text$="QQ阅读"]',
-            '@[text="允许"] <<n * [text$="阿里巴巴"]',
-            '@[text="允许"] <<n * [text$="飞猪旅行"]',
-            '@[text="允许"] <<n * [text$="快影"]',
-            '@[text="允许"] <<n * [text$="芒果TV"]',
-            '@[text="允许"] <<n * [text$="闲鱼"]',
-            '@[text="允许"] <<n * [text$="携程旅行"]',
-            '@[text="允许"] <<n * [text$="一淘"]',
+            '@[text="允许"][clickable=true] - [text="拒绝"] < [vid="buttonPanel"] - * [text$="QQ阅读"][vid="message"]',
+            '@[text="允许"][clickable=true] - [text="拒绝"] < [vid="buttonPanel"] - * [text$="阿里巴巴"][vid="message"]',
+            '@[text="允许"][clickable=true] - [text="拒绝"] < [vid="buttonPanel"] - * [text$="飞猪旅行"][vid="message"]',
+            '@[text="允许"][clickable=true] - [text="拒绝"] < [vid="buttonPanel"] - * [text$="快影"][vid="message"]',
+            '@[text="允许"][clickable=true] - [text="拒绝"] < [vid="buttonPanel"] - * [text$="芒果TV"][vid="message"]',
+            '@[text="允许"][clickable=true] - [text="拒绝"] < [vid="buttonPanel"] - * [text$="闲鱼"][vid="message"]',
+            '@[text="允许"][clickable=true] - [text="拒绝"] < [vid="buttonPanel"] - * [text$="携程旅行"][vid="message"]',
+            '@[text="允许"][clickable=true] - [text="拒绝"] < [vid="buttonPanel"] - * [text$="一淘"][vid="message"]',
           ],
           activityIds: ['.BrowserActivity'],
         },
@@ -37,7 +37,24 @@ export default defineGkdApp({
       rules: [
         {
           matches: [
-            '@[text="立即下载App"] <<n [text="快影APP下载-快手官方视频剪辑神器"]',
+            '[text="快影APP下载-快手官方视频剪辑神器"] >n @View[clickable=true] > [text="立即下载App"]',
+          ],
+          activityIds: ['.BrowserActivity'],
+        },
+      ],
+    },
+    //功能应用类
+    {
+      key: 40,
+      name: '添加小部件-取消',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'app',
+      rules: [
+        {
+          matches: [
+            '[text="立即添加"] - @[text="取消"][clickable=true] < [vid="buttonPanel"] - * [text="添加“全网热榜”小部件"][vid="tv_widget_dialog_title"]',
           ],
           activityIds: ['.BrowserActivity'],
         },
