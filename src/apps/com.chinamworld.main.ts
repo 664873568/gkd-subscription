4b4b74e549b141f0500db353abe170a44c047cfa
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2026-08-19 17:37:01 +0800

    Update com.chinamworld.main.ts

--- src/apps/com.chinamworld.main.ts
+++ src/apps/com.chinamworld.main.ts
@@ -4,6 +4,20 @@ export default defineGkdApp({
   id: 'com.chinamworld.main',
   name: '中国建设银行',
   groups: [
+    {
+      key: 0,
+      name: '低碳生活-一键收取',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'app',
+      rules: [
+        {
+          matches: ['@[text="oneKey"][clickable=true]'],
+          activityIds: ['com.ccb.framework.ui.widget.webview.CcbWebViewActivity'],
+        },
+      ],
+    },
     //首页广告类
     {
       key: 50,
