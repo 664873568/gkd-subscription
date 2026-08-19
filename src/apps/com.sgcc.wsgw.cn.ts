55dad44bfaaca439947a831cc2bb4bcffb9ed2e7
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2026-08-19 17:30:11 +0800

    Update com.sgcc.wsgw.cn.ts

--- src/apps/com.sgcc.wsgw.cn.ts
+++ src/apps/com.sgcc.wsgw.cn.ts
@@ -58,7 +58,8 @@ export default defineGkdApp({
       activityIds: ['com.sgcc.wsgw.mainbundle.ElectricTitleActivity'],
       rules: [
         {
-          matches: [
+          anyMatches: [
+            '[text="2000签到金"] + @ImageButton[clickable=true] + [text~="\\\\（剩余([1-9]|[0-9]*)张\\\\）"]',
             '[text~="\\\\（剩余([1-9]|[0-9][0-9])张\\\\）"] - * > @[text="立即兑换"][clickable=true] - ListView > [text="20元"]',
           ],
         },
