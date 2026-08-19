97358dc6d11a74bc794bb7e1b3021171866e7c99
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2026-08-19 22:42:23 +0800

    Update com.jingdong.app.mall.ts

--- src/apps/com.jingdong.app.mall.ts
+++ src/apps/com.jingdong.app.mall.ts
@@ -4,23 +4,138 @@ export default defineGkdApp({
   id: 'com.jingdong.app.mall',
   name: '京东',
   groups: [
+    //天天砸金蛋
+    {
+      key: 0,
+      name: '天天砸金蛋-去完成',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchDelay: 2000,
+      resetMatch: 'activity',
+      rules: [
+        {
+          key: 0,
+          actionDelay: 1000,
+          anyMatches: [
+            '@View[clickable=true] > View > View > [text="sign icon"]',
+            '@View[clickable=true] > View > View > [desc="sign icon"]',
+          ],
+          activityIds: [
+            '.WebActivity',
+            '.MainFrameActivity',
+          ],
+        },
+        {
+          key: 1,
+          actionDelay: 2000,
+          matches: [
+            'View > View > @[text="去完成"][clickable=true]',
+          ],
+          activityIds: [
+            '.WebActivity',
+            'com.jd.lib.ttt.page.TTTMultiPageActivity',
+          ],
+        },
+        {
+          preKeys: [1],
+          key: 2,
+          action: 'clickCenter',
+          matches: [
+            '@RelativeLayout[clickable=true] >n [text="点击立即返回"] - * [text="已完成"]',//品质不打折，外卖不打烊！-浏览页面得京豆-逛秒杀频道 签到领京豆-逛逛频道领好礼(0/12)-逛自营二手 全品类低价捡漏-浏览页面领京豆(0/2)-逛京东校园签到得京豆-转发好友！瓜分拼手气红包
+          ],
+          activityIds: [
+            '.personel.FloatViewActivity',
+            'com.jd.lib.babel.view.activity.BabelActivity',
+          ],
+        },
+        {
+          preKeys: [1],
+          key: 3,
+          actionDelay: 15000,
+          matches: [
+            'RelativeLayout > @[desc="返回"][clickable=true]',//浏览点击3个商品(0/3)
+            'TextView - @TextView[clickable=true] <<n WebView',//浏览京东金融领京豆(0/3)
+          ],
+          activityIds: ['.WebActivity'],
+        },
+        {
+          preKeys: [1],
+          key: 4,
+          actionDelay: 15000,
+          matches: ['@FrameLayout[clickable=true] > [desc="搜索"]'],//看视频领3折券
+          activityIds: ['com.jd.lib.Discovery.view.DiscoveryActivity'],
+        },
+        {
+          preKeys: [1],
+          key: 5,
+          actionDelay: 15000,
+          matches: ['ViewGroup > LinearLayout > @TextView[text=""][clickable=true] + ImageView'],//逛新品频道 每天领红包-逛排行榜最高得99豆-逛超级明星抽8888红包-逛拍卖领京豆
+          activityIds: ['com.jd.lib.ttt.page.TTTMultiPageActivity'],
+        },
+        {
+          preKeys: [1],
+          key: 6,
+          actionDelay: 15000,
+          matches: ['ViewGroup + ViewGroup + @ViewGroup[clickable=true] > ImageView'],//健康金限时领
+          activityIds: ['com.jingdong.common.jdreactFramework.activities.JDReactNativeCommonActivity'],
+        },
+        {
+          preKeys: [1],
+          key: 7,
+          matches: ['ViewGroup[childCount=2] > ViewGroup + ViewGroup[childCount=2] > @ViewGroup[clickable=true] > ImageView'],//健康金限时领
+          activityIds: ['com.jingdong.common.jdreactFramework.activities.JDReactNativeCommonActivity'],
+        },
+        {
+          preKeys: [1],
+          key: 8,
+          actionDelay: 15000,
+          matches: ['[text="推荐"] <n @ViewGroup[clickable=true] < * + * [text="试用领取"]'],
+          activityIds: ['.MainFrameActivity'],
+        },
+        {
+          preKeys: [1,8],
+          key: 9,
+          matches: ['[text="9.9包邮"] <n @ViewGroup[clickable=true] < * - * [text="推荐"]'],
+          activityIds: ['.MainFrameActivity'],
+        },
+        {
+          key: 10,
+          matches: ['@[text~="砸一下\\\\(剩余[0-9]+次\\\\)"][clickable=true]'],
+          activityIds: [
+            '.WebActivity',
+            '.MainFrameActivity',
+          ],
+        },
+        {
+          preKeys: [10],
+          key: 11,
+          matches: ['@[desc="关闭弹窗"][clickable=true] + View > View > View + TextView[clickable=true]'],
+          activityIds: [
+            '.WebActivity',
+            '.MainFrameActivity',
+          ],
+        },
+      ],
+    },
     //互动游戏
     //https://prodev.m.jd.com/mall/active/3fcyrvLZALNPWCEDRvaZJVrzek8v/index.html
     {
       key: 10,
       name: '互动游戏-攒经验',
       matchRoot: true,
-      actionMaximum: 1,
-      matchTime: 10000,
+      matchDelay: 1000,
       resetMatch: 'activity',
       rules: [
         {
+          key: 0,
           matches: [
             '@TextView[clickable=true] + * View[clickable=true] > [text="明天继续"]',
           ],
           activityIds: ['com.jd.lib.babel.view.activity.BabelActivity'],
         },
         {
+          key: 1,
+          actionDelay: 1000,
           excludeMatches: ['[text="做任务 攒经验"]'],
           matches: [
             '[text="互动游戏"] >n [text="赚京豆"] >n [text="攒经验 ›"][clickable=true]',
@@ -34,8 +149,7 @@ export default defineGkdApp({
       name: '互动游戏-逛一逛-看直播赚钱',
       matchRoot: true,
       actionMaximum: 1,
-      matchDelay: 3000,
-      matchTime: 20000,
+      matchDelay: 2000,
       resetMatch: 'activity',
       rules: [
         {
@@ -49,6 +163,7 @@ export default defineGkdApp({
         {
           preKeys: [0],
           key: 1,
+          actionDelay: 1000,
           anyMatches: [
             '[text="粉丝关注任务"] >n @TextView[clickable=true][index=1]',
             '[text="直播间心愿"] >n @View[clickable=true] > [text="0276048048dc874a"]',
@@ -73,12 +188,11 @@ export default defineGkdApp({
       key: 12,
       name: '互动游戏-逛一逛',
       matchRoot: true,
-      actionMaximum: 1,
       matchDelay: 2000,
-      matchTime: 20000,
       resetMatch: 'activity',
       rules: [
         {
+          key: 0,
           action: 'clickCenter',
           matches: [
             '[text="互动游戏"] >n @View[clickable=true] > [text="逛一逛"]',
@@ -89,6 +203,7 @@ export default defineGkdApp({
           ],
         },
         {
+          key: 1,
           action: 'clickCenter',
           matches: [
             '@RelativeLayout[clickable=true] >n [text="点击立即返回"] - * [text="已完成"]',
@@ -99,6 +214,8 @@ export default defineGkdApp({
           ],
         },
         {
+          key: 2,
+          actionDelay: 1000,
           matches: [
             '[text="互动游戏"] >n @TextView[clickable=true] + * [text="已完成"]',
           ],
@@ -111,12 +228,11 @@ export default defineGkdApp({
       key: 13,
       name: '互动游戏-逛一逛-游戏',
       matchRoot: true,
-      actionMaximum: 1,
       matchDelay: 3000,
-      matchTime: 20000,
       resetMatch: 'activity',
       rules: [
         {
+          key: 0,
           action: 'clickCenter',
           matches: [
             '[text="互动游戏"] >n @View[clickable=true] > [text="逛一逛"]',
@@ -124,6 +240,7 @@ export default defineGkdApp({
           activityIds: ['com.jd.lib.babel.view.activity.BabelActivity'],
         },
         {
+          key: 1,
           actionDelay: 6000,
           matches: [
             '@[desc="关闭"][clickable=true] - [desc="更多"][clickable=true]',
@@ -131,6 +248,8 @@ export default defineGkdApp({
           activityIds: ['com.jingdong.manto.ui.MantoActivityUp1'],
         },
         {
+          key: 2,
+          actionDelay: 1000,
           matches: [
             '@[text="退出游戏"][clickable=true] + [text="添加到桌面"][clickable=true]',
           ],
@@ -142,12 +261,11 @@ export default defineGkdApp({
       key: 14,
       name: '互动游戏-去完成',
       matchRoot: true,
-      actionMaximum: 1,
       matchDelay: 3000,
-      matchTime: 20000,
       resetMatch: 'activity',
       rules: [
         {
+          key: 0,
           action: 'clickCenter',
           matches: [
             '[text="互动游戏"] >n [text~="热卖商品推荐\\\\([0-4]/5\\\\)"] +2 @View[clickable=true] > [text="去完成"]',
@@ -158,6 +276,7 @@ export default defineGkdApp({
           ],
         },
         {
+          key: 1,
           action: 'clickCenter',
           matches: [
             '@RelativeLayout[clickable=true] >n [text="点击立即返回"] - * [text="已完成"]',
@@ -165,6 +284,8 @@ export default defineGkdApp({
           activityIds: ['com.jd.lib.productdetail.ProductDetailActivity'],
         },
         {
+          key: 2,
+          actionDelay: 1000,
           matches: [
             '[text="互动游戏"] >n @TextView[clickable=true] + * [text="已完成"]',
           ],
@@ -177,24 +298,22 @@ export default defineGkdApp({
       name: '互动游戏-领取',
       matchRoot: true,
       matchDelay: 4000,
-      matchTime: 20000,
       resetMatch: 'activity',
+      activityIds: ['com.jd.lib.babel.view.activity.BabelActivity'],
       rules: [
         {
           key: 0,
-          actionDelay: 3000,
           matches: [
             '[text="互动游戏"] >n [text="赚京豆"] >n [text="全部奖励已解锁"] + * >n @[text="领取"][clickable=true]',
           ],
-          activityIds: ['com.jd.lib.babel.view.activity.BabelActivity'],
         },
         {
           preKeys: [0],
           key: 1,
+          actionDelay: 1000,
           matches: [
             '[text="互动游戏"] >n [text="京豆奖励"] +n @[text="开心收下"][clickable=true]',
           ],
-          activityIds: ['com.jd.lib.babel.view.activity.BabelActivity'],
         },
       ],
     },
