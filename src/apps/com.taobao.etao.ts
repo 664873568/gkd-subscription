37eb87783f5e0cb99b256b98485473aa939bc44b
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2026-08-28 13:38:52 +0800

    Update com.taobao.etao.ts

--- src/apps/com.taobao.etao.ts
+++ src/apps/com.taobao.etao.ts
@@ -10,100 +10,71 @@ export default defineGkdApp({
       name: '签到领钱-签到',
       matchRoot: true,
       actionMaximum: 1,
-      matchDelay: 2000,
-      matchTime: 10000,
       resetMatch: 'activity',
+      activityIds: [
+        'com.taobao.sns.tms.CommonTMSActivity',
+        'com.taobao.sns.activity.ISWebViewActivity',
+      ],
       rules: [
         {
           key: 0,
           matches: [
-            '@[text="O1CN01zgOxbV1KnJSkjY2Fc_!!6000000001208-2-tps-150-150"] < * + [text="签到"]',
+            'View > @View[getChild(0).text="O1CN01zgOxbV1KnJSkjY2Fc_!!6000000001208-2-tps-150-150"] + [text="签到"]',
           ],
-          activityIds: ['com.taobao.sns.activity.ISWebViewActivity'],
         },
         {
           preKeys: [0],
           key: 1,
-          actionDelay: 3000,
           matches: [
-            '[text="福狸中心"] >n @[text="TB16l86YLb2gK0jSZK9XXaEgFXa-92-92.png_100x100.jpg_"]',
+            '@View[getChild(0).text="TB16l86YLb2gK0jSZK9XXaEgFXa-92-92.png_100x100.jpg_"] + [text="领取"] + [text="签到"]',
           ],
-          activityIds: ['com.taobao.sns.activity.ISWebViewActivity'],
         },
       ],
     },
     {
       key: 1,
-      name: '签到领钱-逛大促会场',
+      name: '签到领钱-浏览任务',
       matchRoot: true,
-      actionMaximum: 1,
-      matchDelay: 3000,
-      matchTime: 10000,
+      matchDelay: 1000,
       resetMatch: 'activity',
+      activityIds: [
+        'com.taobao.sns.tms.CommonTMSActivity',
+        'com.taobao.sns.activity.ISWebViewActivity',
+      ],
       rules: [
         {
           key: 0,
+          actionDelay: 2000,
           matches: [
-            '@[text="O1CN01s1PguR1l0l6N6MumX_!!6000000004757-2-tps-211-210"] < * + [text="逛大促会场"]',
+            'View > @View[(getChild(0).text="O1CN01s1PguR1l0l6N6MumX_!!6000000004757-2-tps-211-210")&&(getChild(2).text!^="已领")] + [text!~="去中国移动逛逛"]',
           ],
-          activityIds: ['com.taobao.sns.activity.ISWebViewActivity'],
         },
         {
           preKeys: [0],
           key: 1,
           actionDelay: 3000,
           matches: [
-            '[vid="left_panel"] > @[desc="返回"][clickable=true] + * [vid="tvTitle"]',
+            '[vid="left_panel"] > @[desc="返回"][clickable=true] + * [vid="tvTitle"]',//逛大促会场
           ],
-          activityIds: ['com.taobao.sns.tms.CommonTMSActivity'],
-        },
-        {
-          preKeys: [0, 1],
-          key: 2,
-          matches: [
-            '@[text="O1CN01s1PguR1l0l6N6MumX_!!6000000004757-2-tps-211-210"] < * + [text="领取"] + [text="逛大促会场"]',
-          ],
-          activityIds: ['com.taobao.sns.activity.ISWebViewActivity'],
-        },
-      ],
-    },
-    {
-      key: 2,
-      name: '签到领钱-打开',
-      matchRoot: true,
-      actionMaximum: 1,
-      matchDelay: 3000,
-      matchTime: 10000,
-      resetMatch: 'activity',
-      rules: [
-        {
-          fastQuery: true,
-          matches: ['@[text="打开"][clickable=true] - [text="取消"]'],
-          activityIds: ['com.taobao.sns.tms.CommonTMSActivity'],
-        },
-      ],
-    },
-    {
-      key: 3,
-      name: '签到领钱-去点淘领红包',
-      matchRoot: true,
-      actionMaximum: 1,
-      matchDelay: 3000,
-      matchTime: 10000,
-      resetMatch: 'activity',
-      rules: [
-        {
-          key: 0,
-          matches: [
-            '[text="下载/打开APP"] < @View[clickable=true] <<n * - * [text="去点淘领红包"][vid="tvTitle"]',
-          ],
-          activityIds: ['com.taobao.sns.tms.CommonTMSActivity'],
         },
         {
           preKeys: [0],
-          key: 1,
-          matches: ['@[text="打开"] - [text="取消"]'],
-          activityIds: ['com.taobao.sns.tms.CommonTMSActivity'],
+          key: 2,
+          matches: [
+            '[text="下载/打开APP"] < @View[clickable=true] <<n * - * [text="去点淘领红包"][vid="tvTitle"]',//去点淘领红包
+          ],
+        },
+        {
+          preKeys: [0,2],
+          actionDelay: 3000,
+          key: 3,
+          matches: ['@[text="打开"][clickable=true] - [text="取消"]'],
+        },
+        {
+          key: 4,
+          matches: [
+            '@View[getChild(0).text="O1CN01s1PguR1l0l6N6MumX_!!6000000004757-2-tps-211-210"] + [text="领取"] + [text!~="去中国移动逛逛"]',
+          ],
         },
       ],
     },
