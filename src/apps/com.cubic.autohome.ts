f478600bf80b96f78ed8cd31965e9d1e9c4e4c1f
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2026-09-03 21:13:41 +0800

    Add functionality group for homepage ads
    Added a new functionality group for homepage ads with specific rules and actions.

--- src/apps/com.cubic.autohome.ts
+++ src/apps/com.cubic.autohome.ts
@@ -4,6 +4,23 @@ export default defineGkdApp({
   id: 'com.cubic.autohome',
   name: '汽车之家',
   groups: [
+    //功能应用类
+    {
+      key: 40,
+      name: '首页广告-跳过',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'app',
+      rules: [
+        {
+          matches: [
+            '[text="开启通知，重要消息不遗漏"][vid="tv_title"] +n @[vid="re_close"][clickable=true] > [vid="page_operation_icon"]',
+          ],
+          activityIds: ['null'],
+        },
+      ],
+    },
     //首页广告类
     {
       key: 50,
