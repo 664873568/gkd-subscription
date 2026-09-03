400f81b37fa955a5a4cc218f668af20991877d8c
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2026-09-03 21:38:06 +0800

    Update com.ss.android.article.news.ts

--- src/apps/com.ss.android.article.news.ts
+++ src/apps/com.ss.android.article.news.ts
@@ -8,9 +8,7 @@ export default defineGkdApp({
       key: 0,
       name: 'YY-打开今日头条',
       matchRoot: true,
-      actionMaximum: 1,
       matchDelay: 5000,
-      matchTime: 20000,
       resetMatch: 'activity',
       rules: [
         {
