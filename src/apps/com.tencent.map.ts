95a9f20d488410dd4459bfad3c645a3e631fe8f7
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2026-09-09 09:54:13 +0800

    Create com.tencent.map.ts

---
+++ src/apps/com.tencent.map.ts
@@ -0,0 +1,25 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'com.tencent.map',
+  name: '腾讯地图',
+  groups: [
+    //功能应用类
+    {
+      key: 50,
+      name: '用户协议-同意并继续',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'app',
+      rules: [
+        {
+          matches: [
+            '[vid="ll_btns"] > [text="不同意"][vid="reconsider_btn"] + @[text="同意并继续"][vid="sure_btn"][clickable=true]',
+          ],
+          activityIds: ['null'],
+        },
+      ],
+    },
+  ],
+});
