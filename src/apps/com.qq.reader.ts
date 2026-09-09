89ea75b4bc1c25e08e0278fa6946f94307c44650
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2026-09-09 10:03:58 +0800

    Create com.qq.reader.ts

---
+++ src/apps/com.qq.reader.ts
@@ -0,0 +1,25 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'com.qq.reader',
+  name: 'QQ阅读',
+  groups: [
+    //功能应用类
+    {
+      key: 50,
+      name: '欢迎使用QQ阅读-同意',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'app',
+      rules: [
+        {
+          matches: [
+            '[text="不同意"][vid="dont_agree"] - @[text="同意"][vid="agree"][clickable=true] -n * [text="欢迎使用QQ阅读"]',
+          ],
+          activityIds: ['null'],
+        },
+      ],
+    },
+  ],
+});
