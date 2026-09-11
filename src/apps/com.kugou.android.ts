d44155d21631a8355caa30b4090fe765c85c10d8
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2026-09-11 09:47:34 +0800

    Update com.kugou.android.ts

--- src/apps/com.kugou.android.ts
+++ src/apps/com.kugou.android.ts
@@ -4,6 +4,23 @@ export default defineGkdApp({
   id: 'com.kugou.android',
   name: '酷狗音乐',
   groups: [
+    //功能应用类
+    {
+      key: 40,
+      name: '服务条款和隐私保护提示-同意',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'app',
+      rules: [
+        {
+          matches: [
+            '[text="服务条款和隐私保护提示"] +n @[text="同意"][clickable=true] + [text="不同意协议，进入基本功能模式"]',
+          ],
+          activityIds: ['com.kugou.common.privacy.AppStartPrivacyActivity'],
+        },
+      ],
+    },
     //首页广告类
     {
       key: 50,
