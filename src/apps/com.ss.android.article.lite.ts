d10453d8b2a3b649bb07e3616916f8a496642054
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2026-09-12 07:34:50 +0800

    Update com.ss.android.article.lite.ts

--- src/apps/com.ss.android.article.lite.ts
+++ src/apps/com.ss.android.article.lite.ts
@@ -10,13 +10,20 @@ export default defineGkdApp({
       matchRoot: true,
       matchDelay: 5000,
       resetMatch: 'activity',
+      activityIds: ['com.ss.android.ugc.detail.activity.TikTokActivity'],
       rules: [
         {
           anyMatches: [
             '@[desc="返回"][clickable=true] <<n * - MeasureOnceRelativeLayout2 [text="登录领钱"]',
             '@[desc="返回"][clickable=true] <<n * - * MeasureOnceRelativeLayout2[clickable=true] [text="点击赚钱"]',
           ],
-          activityIds: ['com.ss.android.ugc.detail.activity.TikTokActivity'],
+        },
+        {
+          preKeys: [0],
+          key: 1,
+          matches: [
+            '[text="确定要离开？"] +n @[getChild(0).text="仍然离开"][clickable=true] + [getChild(0).text="继续观看"][clickable=true]',
+          ],
         },
       ],
     },
