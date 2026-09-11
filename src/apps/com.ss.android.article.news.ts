c8a77f80fec3edede85a9031e5f33db3471afaa7
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2026-09-11 10:51:39 +0800

    Enhance activity match rules in article news
    Added new rules for handling activity matches in the article news module.

--- src/apps/com.ss.android.article.news.ts
+++ src/apps/com.ss.android.article.news.ts
@@ -12,11 +12,20 @@ export default defineGkdApp({
       resetMatch: 'activity',
       rules: [
         {
+          key: 0,
           anyMatches: [
             '@[desc="返回"][clickable=true] <<n MeasureOnceRelativeLayout2 [text="登录赚钱"]',
             '@[desc="返回"][clickable=true] <<n * - * MeasureOnceRelativeLayout2[clickable=true] [text="登录赚钱"]',
           ],
           activityIds: ['com.ss.android.ugc.detail.activity.TikTokActivity'],
+        },
+        {
+          preKeys:[0],
+          key: 1,
+          matches: [
+            '[text="确定要离开？"] +n @[getChild(0).text="仍然离开"][clickable=true] + [getChild(0).text="继续观看"][clickable=true]',
+          ],
+          activityIds: ['com.ss.android.ugc.detail.activity.TikTokActivity','null'],
         },
       ],
     },
