a347e63d80066491eb353517d85945c62f1136a8
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2026-09-12 15:48:35 +0800

    Update com.taobao.trip.ts

--- src/apps/com.taobao.trip.ts
+++ src/apps/com.taobao.trip.ts
@@ -885,7 +885,7 @@ export default defineGkdApp({
       resetMatch: 'activity',
       rules: [
         {
-          matches: ['@[desc="跳过"][clickable=true]'],
+          matches: ['@[desc="跳过"] <n FrameLayout[clickable=true] - [vid="action_bar_root"]'],
           activityIds: ['.home.HomeActivity'],
         },
       ],
