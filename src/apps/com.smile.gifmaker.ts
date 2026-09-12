2d5cde662a317dd9da09ef5df8ab63f5bf2f74a4
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2026-09-12 15:44:27 +0800

    Update com.smile.gifmaker.ts

--- src/apps/com.smile.gifmaker.ts
+++ src/apps/com.smile.gifmaker.ts
@@ -7,6 +7,22 @@ export default defineGkdApp({
     //功能应用类
     {
       key: 40,
+      name: '欢迎使用快手-同意并继续',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'activity',
+      rules: [
+        {
+          matches: [
+            '[text="欢迎使用快手"][vid="title"] +n [vid="button"] > @[text="同意并继续"][vid="positive"][clickable=true]',
+          ],
+          activityIds: ['com.yxcorp.gifshow.detail.PhotoDetailActivity'],
+        },
+      ],
+    },
+    {
+      key: 41,
       name: '打开推送通知-忽略',
       matchRoot: true,
       actionMaximum: 1,
@@ -15,7 +31,7 @@ export default defineGkdApp({
       rules: [
         {
           matches: [
-            '[text="打开推送通知"][vid="title"] +n [vid="button"] >n @[text="忽略"][vid="negative"][clickable=true] +n [text="去打开"][vid="positive"]',
+            '[text="打开推送通知"][vid="title"] +n [vid="button"] >n @[text="忽略"][vid="negative"][clickable=true]',
           ],
           activityIds: ['com.yxcorp.gifshow.detail.PhotoDetailActivity'],
         },
