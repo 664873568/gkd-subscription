1b19991f1dd00343fdb2928aa7a2c9f3f72d89ff
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2026-09-12 15:16:18 +0800

    Create com.xs.fm.ts

---
+++ src/apps/com.xs.fm.ts
@@ -0,0 +1,23 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'com.xs.fm',
+  name: '番茄畅听',
+  groups: [
+    //功能应用类
+    {
+      key: 40,
+      name: '个人信息保护指引-同意',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'app',
+      rules: [
+        {
+          matches: ['[text="个人信息保护指引"] +n * [text="同意"][clickable=true]'],
+          activityIds: ['null'],
+        },
+      ],
+    },
+  ],
+});
