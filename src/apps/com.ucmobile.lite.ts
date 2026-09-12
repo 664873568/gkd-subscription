916f8ef824850662e7c555bb9af69fd4527fbb6c
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2026-09-12 15:31:47 +0800

    Create com.ucmobile.lite.ts

---
+++ src/apps/com.ucmobile.lite.ts
@@ -0,0 +1,25 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'com.ucmobile.lite',
+  name: 'UC浏览器极速版',
+  groups: [
+    //功能应用类
+    {
+      key: 40,
+      name: '个人信息保护指引-同意',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'app',
+      rules: [
+        {
+          matches: [
+            '[text="个人信息保护指引"][vid="title"] +n [text="不同意"][vid="not_agree"] + @[text="同意"][vid="agree"][clickable=true]',
+          ],
+          activityIds: ['null'],
+        },
+      ],
+    },
+  ],
+});
