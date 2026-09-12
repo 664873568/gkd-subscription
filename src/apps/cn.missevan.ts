844b0a35736c9723996cbb9795d3082bedb4288a
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2026-09-12 15:10:36 +0800

    Update cn.missevan.ts

--- src/apps/cn.missevan.ts
+++ src/apps/cn.missevan.ts
@@ -4,6 +4,23 @@ export default defineGkdApp({
   id: 'cn.missevan',
   name: '猫耳FM',
   groups: [
+    //功能应用类
+    {
+      key: 40,
+      name: '用户协议与隐私政策提示-同意',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'app',
+      rules: [
+        {
+          matches: [
+            '[text="用户协议与隐私政策提示"][vid="title"] +n [text="不同意"][vid="disagree"] + @[text="同意"][vid="agree"][clickable=true]',
+          ],
+          activityIds: ['null'],
+        },
+      ],
+    },
     //首页广告类
     {
       key: 50,
