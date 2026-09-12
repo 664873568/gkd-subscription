3e808f4ed863226827e3eea3df321a20ebf3a6cd
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2026-09-12 15:25:26 +0800

    Update com.tencent.karaoke.ts

--- src/apps/com.tencent.karaoke.ts
+++ src/apps/com.tencent.karaoke.ts
@@ -4,6 +4,23 @@ export default defineGkdApp({
   id: 'com.tencent.karaoke',
   name: '全民K歌',
   groups: [
+    //功能应用类
+    {
+      key: 40,
+      name: '用户协议及隐私保护-同意',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'app',
+      rules: [
+        {
+          matches: [
+            '[getChild(0).getChild(0).text="用户协议及隐私保护"] + * @[text="同意"][clickable=true]',
+          ],
+          activityIds: ['null'],
+        },
+      ],
+    },
     //首页广告类
     {
       key: 50,
