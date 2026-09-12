16271e14405a9e53488ceee699bbbad5717f31de
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2026-09-12 15:37:25 +0800

    Update com.sup.android.superb.ts

--- src/apps/com.sup.android.superb.ts
+++ src/apps/com.sup.android.superb.ts
@@ -7,6 +7,23 @@ export default defineGkdApp({
     //功能应用类
     {
       key: 40,
+      name: '个人信息保护指引-同意',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchDelay: 10000,
+      resetMatch: 'activity',
+      rules: [
+        {
+          action: 'back',
+          matches: [
+            '[getChild(0).text="个人信息保护指引"] + LinearLayout > [text="不同意"] + @[text="同意"][clickable=true]',
+          ],
+          activityIds: ['com.sup.android.base.MainActivity'],
+        },
+      ],
+    },
+    {
+      key: 41,
       name: '开启推送通知-以后再说',
       matchRoot: true,
       actionMaximum: 1,
@@ -16,7 +33,7 @@ export default defineGkdApp({
         {
           action: 'back',
           matches: [
-            '[text="开启推送通知"] +n * > @[text="以后再说"][clickable=true] + [text="马上开启"][clickable=true]',
+            '[text="开启推送通知"] +n LinearLayout > @[text="以后再说"][clickable=true] + [text="马上开启"][clickable=true]',
           ],
           activityIds: ['com.sup.android.base.MainActivity'],
         },
