12bf4865cd7442970ef719b39f555d039f2747b9
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2026-09-12 15:07:16 +0800

    Add new group for user consent in theater app

--- src/apps/com.kwai.theater.ts
+++ src/apps/com.kwai.theater.ts
@@ -7,6 +7,22 @@ export default defineGkdApp({
     //功能应用类
     {
       key: 40,
+      name: '欢迎使用喜番免费短剧-同意',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'activity',
+      rules: [
+        {
+          matches: [
+            '[text="欢迎使用喜番免费短剧"][vid="privacy_title"] < * +n * > @[vid="ksad_tube_privacy_confirm_view"][clickable=true] > [text="同意"][vid="ksad_tube_dialog_confirm_text"]',
+          ],
+          activityIds: ['.api.component.HomeActivity'],
+        },
+      ],
+    },
+    {
+      key: 41,
       name: '打开通知-取消',
       matchRoot: true,
       actionMaximum: 1,
