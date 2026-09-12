9c0a12096e98091b7fd0c563df46ade9ff933e56
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2026-09-12 15:19:56 +0800

    Update com.tencent.qqmusic.ts

--- src/apps/com.tencent.qqmusic.ts
+++ src/apps/com.tencent.qqmusic.ts
@@ -7,6 +7,22 @@ export default defineGkdApp({
     //功能应用类
     {
       key: 40,
+      name: '用户协议和隐私政策概要-同意',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'app',
+      rules: [
+        {
+          matches: [
+            '[getChild(0).text="用户协议和隐私政策概要"] + @[text="同意"][clickable=true]',
+          ],
+          activityIds: ['null'],
+        },
+      ],
+    },
+    {
+      key: 41,
       name: '流量提醒-×',
       matchRoot: true,
       actionMaximum: 1,
