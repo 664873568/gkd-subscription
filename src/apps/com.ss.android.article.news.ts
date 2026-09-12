14c9a8e545cdb073ad131a943c9712b9f24fa135
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2026-09-12 16:11:38 +0800

    Update com.ss.android.article.news.ts

--- src/apps/com.ss.android.article.news.ts
+++ src/apps/com.ss.android.article.news.ts
@@ -8,12 +8,13 @@ export default defineGkdApp({
       key: 0,
       name: 'YY-打开今日头条',
       matchRoot: true,
-      matchDelay: 5000,
+      matchDelay: 1000,
       resetMatch: 'activity',
       activityIds: ['com.ss.android.ugc.detail.activity.TikTokActivity'],
       rules: [
         {
           key: 0,
+          actionDelay: 5000,
           anyMatches: [
             '@[desc="返回"][clickable=true] <<n MeasureOnceRelativeLayout2 [text="登录赚钱"]',
             '@[desc="返回"][clickable=true] <<n * - * MeasureOnceRelativeLayout2[clickable=true] [text="登录赚钱"]',
@@ -25,6 +26,20 @@ export default defineGkdApp({
           matches: [
             '[text="确定要离开？"] +n @[getChild(0).text="仍然离开"][clickable=true] + [getChild(0).text="继续观看"][clickable=true]',
           ],
+        },
+      ],
+    },
+    //功能应用类
+    {
+      key: 40,
+      name: '个人信息保护指引-同意',
+      matchRoot: true,
+      matchDelay: 1000,
+      resetMatch: 'app',
+      rules: [
+        {
+          anyMatches: ['[text="个人信息保护指引"] +n @[text="同意"][clickable=true]'],
+          activityIds: ['null'],
         },
       ],
     },
