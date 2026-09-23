7526e6038649de4995459b2644b87fd4c2bc8e52
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2026-09-24 07:59:05 +0800

    Update match text in rules for activity

--- src/apps/com.dianping.v1.ts
+++ src/apps/com.dianping.v1.ts
@@ -40,7 +40,7 @@ export default defineGkdApp({
       resetMatch: 'activity',
       rules: [
         {
-          matches: ['@FrameLayout[clickable=true] > [text*="餐奖励"]'],
+          matches: ['@FrameLayout[clickable=true] > [text*="奖励"]'],
           activityIds: ['com.dianping.nova.picasso.DPPicassoBoxActivity'],
         },
       ],
