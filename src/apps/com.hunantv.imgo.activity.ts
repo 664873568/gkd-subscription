4e28860e5e48d152361b3a8a161727a515ed3d28
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2026-09-24 08:03:32 +0800

    Add functionality to skip homepage ad
    Added a new functionality application to skip the homepage ad with specific matching rules.

--- src/apps/com.hunantv.imgo.activity.ts
+++ src/apps/com.hunantv.imgo.activity.ts
@@ -4,6 +4,23 @@ export default defineGkdApp({
   id: 'com.hunantv.imgo.activity',
   name: '芒果TV',
   groups: [
+    //功能应用类
+    {
+      key: 40,
+      name: '首页-跳过',
+      matchRoot: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'app',
+      rules: [
+        {
+          matches: [
+            '[text="个人信息保护政策"][vid="tvTitle"] +n [vid="btnLayout"] > [vid="ll_two_buttons"] > [vid="rightBtnFrame"] > @[text="同意并继续"][vid="tvRightBtn"][clickable=true]',
+          ],
+          activityIds: ['com.mgtv.ui.ad.AdSchemaSplashActivity'],
+        },
+      ],
+    },
     //首页广告类
     {
       key: 50,
@@ -16,7 +33,7 @@ export default defineGkdApp({
         {
           anyMatches: [
             '@[text~="跳过 [0-9]"][clickable=true]',
-            //'@[text="跳过"][vid="mgmi_ad_skip_text"] <n [vid="layout_boot_skip"]',
+            '@[text="跳过"][vid="mgmi_ad_skip_text"] <n [vid="layout_boot_skip"]',
           ],
           activityIds: ['com.mgtv.ui.ad.AdSchemaSplashActivity'],
         },
