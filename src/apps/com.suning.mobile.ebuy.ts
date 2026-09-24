ad7f5380d25e7246614abd4833291ea449bc125b
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2026-09-25 03:20:40 +0800

    Update com.suning.mobile.ebuy.ts

--- src/apps/com.suning.mobile.ebuy.ts
+++ src/apps/com.suning.mobile.ebuy.ts
@@ -6,13 +6,14 @@ export default defineGkdApp({
   groups: [
     {
       key: 0,
-      name: '星图金融-返回',
+      name: '星图金融-去易购领云钻',
       matchRoot: true,
       actionMaximum: 1,
-      matchDelay: 10000,
+      matchDelay: 1000,
       resetMatch: 'activity',
       rules: [
         {
+          actionDelay: 5000,
           matches: [
             '@[desc="返回"][vid="btn_back"][clickable=true] < [vid="layout_ucwv_back"] + [text="云钻魔法狮"][vid="title"]',
           ],
