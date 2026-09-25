b60f6045656b936a0853384e4c403590cd7fde65
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2026-09-25 08:48:43 +0800

    Update com.jd.jrapp.ts

--- src/apps/com.jd.jrapp.ts
+++ src/apps/com.jd.jrapp.ts
@@ -663,7 +663,7 @@ export default defineGkdApp({
         {
           preKeys: [0],
           key: 1,
-          anyMmatches: [
+          anyMatches: [
             '@[getChild(0).text="返回签到"][clickable=true] - * [text="readMissionDown"]',
             '@[getChild(0).text="返回「打卡拿好礼」"][clickable=true] - * [text="readMissionDown"]',
           ],
