cbddde0d2f66bda841f3b43cb142de39db8758d5
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2026-09-25 08:47:15 +0800

    Rename 'matches' to 'anyMmatches' in jrapp.ts

--- src/apps/com.jd.jrapp.ts
+++ src/apps/com.jd.jrapp.ts
@@ -663,8 +663,9 @@ export default defineGkdApp({
         {
           preKeys: [0],
           key: 1,
-          matches: [
+          anyMmatches: [
             '@[getChild(0).text="返回签到"][clickable=true] - * [text="readMissionDown"]',
+            '@[getChild(0).text="返回「打卡拿好礼」"][clickable=true] - * [text="readMissionDown"]',
           ],
         },
         {
