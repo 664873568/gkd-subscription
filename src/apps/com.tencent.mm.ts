29e21a7fc5449692160eb7871cf21c59b72bbe51
蓝蝶绯翼 <50593310+664873568@users.noreply.github.com>
2026-09-26 14:20:05 +0800

    Update com.tencent.mm.ts

--- src/apps/com.tencent.mm.ts
+++ src/apps/com.tencent.mm.ts
@@ -6,7 +6,7 @@ export default defineGkdApp({
   groups: [
     {
       key: 0,
-      name: '中国电信湖北客服-星币兑好礼-10元话费',
+      name: '中国电信湖北客服-星币兑好礼-*元话费',
       matchRoot: true,
       matchTime: 10000,
       resetMatch: 'activity',
@@ -18,14 +18,12 @@ export default defineGkdApp({
             '[text="星币兑换温馨提示"] +n View > [text="取消"] + [text="确认兑换"]',
             '[text="短信验证码"] + View > View + [text="获取验证码"]',
           ],
-          matches: ['[getChild(0).text="10元话费"] + View > [text="兑换"]'],
+          matches: ['[getChild(0).text$="元话费"] + View > [text="兑换"]'],
         },
         {
           preKeys: [0],
           key: 1,
-          matches: [
-            '[text="星币兑换温馨提示"] +n View > [text="取消"] + [text="确认兑换"]',
-          ],
+          matches: ['[text="星币兑换温馨提示"] +n View > [text="取消"] + [text="确认兑换"]'],
         },
         {
           preKeys: [1],
@@ -36,7 +34,7 @@ export default defineGkdApp({
     },
     {
       key: 1,
-      name: '中国电信湖北客服-星币兑好礼-5元翼支付权益金',
+      name: '中国电信湖北客服-星币兑好礼-*元翼支付权益金',
       matchRoot: true,
       matchTime: 10000,
       resetMatch: 'activity',
@@ -48,16 +46,12 @@ export default defineGkdApp({
             '[text="星币兑换温馨提示"] +n View > [text="取消"] + [text="确认兑换"]',
             '[text="短信验证码"] + View > View + [text="获取验证码"]',
           ],
-          matches: [
-            '[getChild(0).text="5元翼支付权益金"] + View > [text="兑换"]',
-          ],
+          matches: ['[getChild(0).text$="元翼支付权益金"] + View > [text="兑换"]'],
         },
         {
           preKeys: [0],
           key: 1,
-          matches: [
-            '[text="星币兑换温馨提示"] +n View > [text="取消"] + [text="确认兑换"]',
-          ],
+          matches: ['[text="星币兑换温馨提示"] +n View > [text="取消"] + [text="确认兑换"]'],
         },
         {
           preKeys: [1],
@@ -72,10 +66,15 @@ export default defineGkdApp({
       matchRoot: true,
       matchTime: 10000,
       resetMatch: 'activity',
+      activityIds: ['.plugin.appbrand.ui.AppBrandUI00'],
       rules: [
         {
+          key: 0,
           matches: ['[getChild(0).getChild(0).text="广告"] + @Image'],
-          activityIds: ['.plugin.appbrand.ui.AppBrandUI00'],
+        },
+        {
+          key: 1,
+          matches: ['@Image + Image + [text="恭喜您，兑换成功！"]'],
         },
       ],
     },
@@ -128,11 +127,11 @@ export default defineGkdApp({
     },
     {
       key: 12,
-      name: '迅雷-看广告-已获得奖励',
+      name: '迅雷-看广告',
       matchRoot: true,
-      actionMaximum: 1,
       matchDelay: 1000,
       resetMatch: 'activity',
+      activityIds: ['.plugin.appbrand.ui.AppBrandUI00'],
       rules: [
         {
           key: 0,
@@ -140,40 +139,15 @@ export default defineGkdApp({
           matches: [
             '@[text="领取"] <<n * [text="看广告免费领取迅雷云添加次数"]',
           ],
-          activityIds: ['.plugin.appbrand.ui.AppBrandUI00'],
         },
         {
           preKeys: 0,
           key: 1,
           actionDelay: 30000,
           matches: ['@[text="关闭"] <<n * [text="已获得奖励"]'],
-          activityIds: ['.plugin.appbrand.ui.AppBrandUI00'],
         },
       ],
     },
-    {
-      key: 13,
-      name: '迅雷-已获得奖励-看广告',
-      matchRoot: true,
-      actionMaximum: 1,
-      matchDelay: 1000,
-      resetMatch: 'activity',
-      rules: [
-        {
-          key: 0,
-          actionDelay: 30000,
-          matches: ['@[text="关闭"] <<n * [text="已获得奖励"]'],
-          activityIds: ['.plugin.appbrand.ui.AppBrandUI00'],
-        },
-        {
-          preKeys: 0,
-          key: 1,
-          excludeMatches: ['@[text="关闭"] <<n * [text="已获得奖励"]'],
-          matches: [
-            '@[text="领取"] <<n * [text="看广告免费领取迅雷云添加次数"]',
-          ],
-          activityIds: ['.plugin.appbrand.ui.AppBrandUI00'],
-        },
       ],
     },
   ],
